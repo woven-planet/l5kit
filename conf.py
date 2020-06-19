10016779d498ae53cd756b719259e802e311cb4c
lucabergamini <lucabergamini@lyft.com>
2020-06-19 10:13:00 +0200

    update sphinx build

--- conf.py
+++ conf.py
@@ -46,7 +46,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['.env/*', '.venv/*', '*/l5kit.egg-info/*']
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -59,7 +59,7 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = []
 
 # -- Markdown support
 
