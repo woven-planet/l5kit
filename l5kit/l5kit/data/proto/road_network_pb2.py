a75e9fde76348d57dcfef33dc5755426ab31e86b
yaweiye <yye@lyft.com>
2020-06-30 18:47:03 +0000

    formatting

--- l5kit/l5kit/data/proto/road_network_pb2.py
+++ l5kit/l5kit/data/proto/road_network_pb2.py
@@ -3,16 +3,15 @@
 # source: road_network.proto
 
 from google.protobuf import descriptor as _descriptor
+from google.protobuf import descriptor_pb2 as google_dot_protobuf_dot_descriptor__pb2
+from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 from google.protobuf import message as _message
 from google.protobuf import reflection as _reflection
 from google.protobuf import symbol_database as _symbol_database
+
 # @@protoc_insertion_point(imports)
 
 _sym_db = _symbol_database.Default()
-
-
-from google.protobuf import descriptor_pb2 as google_dot_protobuf_dot_descriptor__pb2
-from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 
 
 DESCRIPTOR = _descriptor.FileDescriptor(
