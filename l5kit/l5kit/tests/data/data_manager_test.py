cb5535de9c0400fc050b54297690474bf4ab94fd
lucabergamini <lucabergamini@lyft.com>
2020-07-17 12:17:40 +0200

    rm unittest tests/data

--- l5kit/l5kit/tests/data/data_manager_test.py
+++ l5kit/l5kit/tests/data/data_manager_test.py
@@ -1,20 +1,20 @@
-import os
-import unittest
-from tempfile import TemporaryDirectory
+from pathlib import Path
+
+import pytest
 
 from l5kit.data import LocalDataManager
 
 
-class TestDataManager(unittest.TestCase):
-    def test_require_existing_file(self) -> None:
-        with TemporaryDirectory() as directory:
-            p = os.path.join(directory, "my_file.txt")
-            open(p, "w").write("hello")
+def test_require_existing_file(tmp_path: Path) -> None:
+    print(tmp_path)
+    p = tmp_path / "my_file.txt"
+    open(str(p), "w").write("hello")
 
-            dm_local = LocalDataManager(directory)
-            self.assertEqual(dm_local.require("my_file.txt"), p)
+    dm_local = LocalDataManager(tmp_path)
+    assert dm_local.require("my_file.txt") == str(p)
 
-    def test_require_non_existing_file(self) -> None:
-        dm_local = LocalDataManager("")  # cur path
-        with self.assertRaises(FileNotFoundError):
-            dm_local.require("some-file-that-doesnt-exist.txt")
+
+def test_require_non_existing_file() -> None:
+    dm_local = LocalDataManager("")  # cur path
+    with pytest.raises(FileNotFoundError):
+        dm_local.require("some-file-that-doesnt-exist.txt")
