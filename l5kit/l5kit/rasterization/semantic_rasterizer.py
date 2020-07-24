4760d3b7c2cb0b85c054d734b78ad5a389197d58
lucabergamini <lucabergamini@lyft.com>
2020-07-24 11:21:10 +0200

    refactor render_semantic_map

--- l5kit/l5kit/rasterization/semantic_rasterizer.py
+++ l5kit/l5kit/rasterization/semantic_rasterizer.py
@@ -10,66 +10,39 @@ from ..geometry import rotation33_as_yaw, transform_point
 from .rasterizer import Rasterizer
 
 
-def render_semantic_map(
-    semantic_map: dict, x: float, y: float, yaw: float, raster_size: Tuple[int, int], pixel_size: np.ndarray
-) -> np.ndarray:
-    """Renders the semantic map at given x,y coordinate with rotation ``yaw``.
+# TODO refactor and explain what's happening
+def map_to_image(
+    x: float,
+    y: float,
+    yaw: float,
+    px: np.ndarray,
+    py: np.ndarray,
+    raster_size: Tuple[int, int],
+    pixel_size: np.ndarray,
+) -> Tuple[np.ndarray, np.ndarray]:
+    rx = (np.cos(yaw) * (px - x) - np.sin(yaw) * (py - y)) / pixel_size[0] + 0.5 * raster_size[0]
+    ry = (np.sin(yaw) * (px - x) + np.cos(yaw) * (py - y)) / pixel_size[1] + 0.5 * raster_size[1]
+    return rx.astype(int) * 256, ry.astype(int) * 256
+
+
+def elements_within_radius(x: float, y: float, bounds: np.ndarray, radius: float) -> np.ndarray:
+    """
+    Get indices of elements for which bounds are inside a radius from center (x,y)
 
     Args:
-        semantic_map (dict): Semantic map dict.
-        x (float): X coordinate to center crop from.
-        y (float): Y coordinate to center crop from.
-        yaw (float): Rotation of the cropped image.
-        raster_size (Tuple[int, int]): Size of the image to render.
-        pixel_size (np.ndarray): The scale of one pixel in the real world (in meters).
+        x (float): center x
+        y (float): center y
+        bounds (np.ndarray): array of shape Nx2x2 [[x_min,y_min],[x_max, y_max]]
+        radius (float): radius value
 
     Returns:
-        np.ndarray: RGB raster.
+        np.ndarray: indices of elements inside radius from center
     """
-
-    img = 255 * np.ones(shape=(raster_size[1], raster_size[0], 3), dtype=np.uint8)
-
-    # TODO refactor (scope variables) and explain what's happening
-    def map_to_image(px: np.ndarray, py: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
-        rx = (np.cos(yaw) * (px - x) - np.sin(yaw) * (py - y)) / pixel_size[0] + 0.5 * raster_size[0]
-        ry = (np.sin(yaw) * (px - x) + np.cos(yaw) * (py - y)) / pixel_size[1] + 0.5 * raster_size[1]
-        return rx.astype(int) * 256, ry.astype(int) * 256
-
-    def elements_within_radius(bounds: dict, radius: np.ndarray) -> np.ndarray:
-        x_min_in = x > bounds[:, 0, 0] - radius
-        y_min_in = y > bounds[:, 0, 1] - radius
-        x_max_in = x < bounds[:, 1, 0] + radius
-        y_max_in = y < bounds[:, 1, 1] + radius
-        return np.nonzero(x_min_in & y_min_in & x_max_in & y_max_in)[0]
-
-    r = np.linalg.norm(raster_size * pixel_size)
-
-    # plot lanes
-    lines = []
-    for i in elements_within_radius(semantic_map["lanes_bounds"], r):
-        lane = semantic_map["lanes"][i]
-        x1, y1 = map_to_image(lane["xyz_left"][0], lane["xyz_left"][1])
-        x2, y2 = map_to_image(lane["xyz_right"][0], lane["xyz_right"][1])
-        pts1 = np.vstack((x1, y1)).T
-        pts2 = np.vstack((x2, y2)).T
-        lines.append(pts1)
-        lines.append(pts2)
-        poly = np.vstack((pts1, np.flip(pts2, 0)))
-        cv2.fillPoly(img, [poly], (17, 17, 31), lineType=cv2.LINE_AA, shift=8)
-
-    cv2.polylines(img, lines, False, (255, 217, 82), lineType=cv2.LINE_AA, shift=8)
-
-    # plot crosswalks
-    crosswalks = []
-    for i in elements_within_radius(semantic_map["crosswalks_bounds"], r):
-        crosswalk = semantic_map["crosswalks"][i]
-        x1, y1 = map_to_image(crosswalk["xyz"][0], crosswalk["xyz"][1])
-        pts = np.vstack((x1, y1)).T
-        crosswalks.append(pts)
-
-    cv2.polylines(img, crosswalks, True, (255, 117, 69), lineType=cv2.LINE_AA, shift=8)
-
-    return img
+    x_min_in = x > bounds[:, 0, 0] - radius
+    y_min_in = y > bounds[:, 0, 1] - radius
+    x_max_in = x < bounds[:, 1, 0] + radius
+    y_max_in = y < bounds[:, 1, 1] + radius
+    return np.nonzero(x_min_in & y_min_in & x_max_in & y_max_in)[0]
 
 
 class SemanticRasterizer(Rasterizer):
@@ -129,16 +102,58 @@ class SemanticRasterizer(Rasterizer):
         rot_m = np.asarray([[np.cos(ego_yaw), np.sin(ego_yaw)], [-np.sin(ego_yaw), np.cos(ego_yaw)]])
         ego_offset_meters = rot_m @ ego_offset_meters
 
-        sem_im = render_semantic_map(
-            self.semantic_map,
-            x + ego_offset_meters[0],
-            y + ego_offset_meters[1],
-            ego_yaw,
-            raster_size=self.raster_size,
-            pixel_size=self.pixel_size,
-        )
-
+        sem_im = self.render_semantic_map(x + ego_offset_meters[0], y + ego_offset_meters[1], ego_yaw)
         return sem_im.astype(np.float32) / 255
+
+    def render_semantic_map(self, x: float, y: float, yaw: float) -> np.ndarray:
+        """Renders the semantic map at given x,y coordinate with rotation ``yaw``.
+
+        Args:
+            x (float): X coordinate to center crop from.
+            y (float): Y coordinate to center crop from.
+            yaw (float): Rotation of the cropped image.
+
+        Returns:
+            np.ndarray: RGB raster
+
+        """
+
+        img = 255 * np.ones(shape=(self.raster_size[1], self.raster_size[0], 3), dtype=np.uint8)
+        r = float(np.linalg.norm(self.raster_size * self.pixel_size))
+
+        # plot lanes
+        lines = []
+        for idx in elements_within_radius(x, y, self.semantic_map["lanes_bounds"], r):
+            lane = self.semantic_map["lanes"][idx]
+
+            x1, y1 = map_to_image(
+                x, y, yaw, lane["xyz_left"][0], lane["xyz_left"][1], self.raster_size, self.pixel_size
+            )
+            x2, y2 = map_to_image(
+                x, y, yaw, lane["xyz_right"][0], lane["xyz_right"][1], self.raster_size, self.pixel_size
+            )
+            pts1 = np.vstack((x1, y1)).T
+            pts2 = np.vstack((x2, y2)).T
+            lines.append(pts1)
+            lines.append(pts2)
+            poly = np.vstack((pts1, np.flip(pts2, 0)))
+            cv2.fillPoly(img, [poly], (17, 17, 31), lineType=cv2.LINE_AA, shift=8)
+
+        cv2.polylines(img, lines, False, (255, 217, 82), lineType=cv2.LINE_AA, shift=8)
+
+        # plot crosswalks
+        crosswalks = []
+        for idx in elements_within_radius(x, y, self.semantic_map["crosswalks_bounds"], r):
+            crosswalk = self.semantic_map["crosswalks"][idx]
+            x1, y1 = map_to_image(
+                x, y, yaw, crosswalk["xyz"][0], crosswalk["xyz"][1], self.raster_size, self.pixel_size
+            )
+            pts = np.vstack((x1, y1)).T
+            crosswalks.append(pts)
+
+        cv2.polylines(img, crosswalks, True, (255, 117, 69), lineType=cv2.LINE_AA, shift=8)
+
+        return img
 
     def to_rgb(self, in_im: np.ndarray, **kwargs: dict) -> np.ndarray:
         return (in_im * 255).astype(np.uint8)
