ab1413a5d073d4016953b35580b87450e0d7ec4d
lucabergamini <lucabergamini@lyft.com>
2020-07-28 19:44:14 +0200

    Lberg/semantic pose (#82)
    * first refactor of semantic rast
    
    * remove legacy function
    
    * remove old TODO
    
    * refactor and add comments
    
    * Refactor SemanticRasterizer
    
    clarify cv2 shift using a function
    
    * reduce ROI
    
    * rename function and param
    
    * add test for bound function
    
    * enable tests also on other branches PRs
    
    * enable tests also on other branches PRs
    
    * remove comment type
    
    * address review

--- l5kit/l5kit/rasterization/semantic_rasterizer.py
+++ l5kit/l5kit/rasterization/semantic_rasterizer.py
@@ -2,47 +2,51 @@ from typing import List, Optional, Tuple
 
 import cv2
 import numpy as np
-import pymap3d as pm
 
 from ..data import proto_to_semantic_map
 from ..data.proto.road_network_pb2 import MapFragment
-from ..geometry import rotation33_as_yaw, transform_point, world_to_image_pixels_matrix
+from ..geometry import rotation33_as_yaw, transform_point, transform_points, world_to_image_pixels_matrix
 from .rasterizer import Rasterizer
 
-
-# TODO refactor and explain what's happening
-def map_to_image(
-    x: float,
-    y: float,
-    yaw: float,
-    px: np.ndarray,
-    py: np.ndarray,
-    raster_size: Tuple[int, int],
-    pixel_size: np.ndarray,
-) -> Tuple[np.ndarray, np.ndarray]:
-    rx = (np.cos(yaw) * (px - x) - np.sin(yaw) * (py - y)) / pixel_size[0] + 0.5 * raster_size[0]
-    ry = (np.sin(yaw) * (px - x) + np.cos(yaw) * (py - y)) / pixel_size[1] + 0.5 * raster_size[1]
-    return rx.astype(int) * 256, ry.astype(int) * 256
+# sub-pixel drawing precision constants
+CV2_SHIFT = 8  # how many bits to shift in drawing
 
 
-def elements_within_radius(x: float, y: float, bounds: np.ndarray, radius: float) -> np.ndarray:
+def elements_within_bounds(center: np.ndarray, bounds: np.ndarray, half_extent: float) -> np.ndarray:
     """
-    Get indices of elements for which bounds are inside a radius from center (x,y)
+    Get indices of elements for which the bounding box described by bounds intersects the one defined around
+    center (square with side 2*half_side)
 
     Args:
-        x (float): center x
-        y (float): center y
+        center (float): XY of the center
         bounds (np.ndarray): array of shape Nx2x2 [[x_min,y_min],[x_max, y_max]]
-        radius (float): radius value
+        half_extent (float): half the side of the bounding box centered around center
 
     Returns:
         np.ndarray: indices of elements inside radius from center
     """
-    x_min_in = x > bounds[:, 0, 0] - radius
-    y_min_in = y > bounds[:, 0, 1] - radius
-    x_max_in = x < bounds[:, 1, 0] + radius
-    y_max_in = y < bounds[:, 1, 1] + radius
+    x_center, y_center = center
+
+    x_min_in = x_center > bounds[:, 0, 0] - half_extent
+    y_min_in = y_center > bounds[:, 0, 1] - half_extent
+    x_max_in = x_center < bounds[:, 1, 0] + half_extent
+    y_max_in = y_center < bounds[:, 1, 1] + half_extent
     return np.nonzero(x_min_in & y_min_in & x_max_in & y_max_in)[0]
+
+
+def cv2_subpixel(coords: np.ndarray) -> np.ndarray:
+    """
+    Cast coordinates to numpy.int but keep fractional part by previously multiplying by 2**CV2_SHIFT
+
+    Args:
+        coords (np.ndarray): XY coords as float
+
+    Returns:
+        np.ndarray: XY coords as int for cv2 shift draw
+    """
+    coords = coords * 2 ** CV2_SHIFT
+    coords = coords.astype(np.int)
+    return coords
 
 
 class SemanticRasterizer(Rasterizer):
@@ -69,7 +73,9 @@ class SemanticRasterizer(Rasterizer):
             mapfrag = MapFragment()
             mapfrag.ParseFromString(infile.read())
 
-        self.semantic_map = proto_to_semantic_map(mapfrag)
+        ecef_to_pose = np.linalg.inv(self.pose_to_ecef)
+
+        self.semantic_map = proto_to_semantic_map(mapfrag, ecef_to_pose)
         self.elements_lookup = {el.id.id.decode("utf-8"): el for el in mapfrag.elements}
 
     def rasterize(
@@ -92,66 +98,57 @@ class SemanticRasterizer(Rasterizer):
             self.raster_size, self.pixel_size, ego_translation, ego_yaw, self.ego_center,
         )
 
-        # get pose of center pixel
+        # get XY of center pixel in world coordinates
         center_pixel = np.asarray(self.raster_size) * (0.5, 0.5)
-        world_translation = transform_point(center_pixel, np.linalg.inv(world_to_image_space))
-        world_translation = np.append(world_translation, ego_translation[2])
+        center_world = transform_point(center_pixel, np.linalg.inv(world_to_image_space))
 
-        # move pose to enu frame the semantic map is stored in (pose->ecef, ecef->enu)
-        xyz = transform_point(world_translation, self.pose_to_ecef)
-        x, y, z = pm.ecef2enu(xyz[0], xyz[1], xyz[2], self.semantic_map["lat"], self.semantic_map["lon"], 0)
-
-        # TODO (lberg): understand -yaw here in relation with map_to_image
-        sem_im = self.render_semantic_map(float(x), float(y), -ego_yaw)
+        sem_im = self.render_semantic_map(center_world, world_to_image_space)
         return sem_im.astype(np.float32) / 255
 
-    def render_semantic_map(self, x: float, y: float, yaw: float) -> np.ndarray:
-        """Renders the semantic map at given x,y coordinate with rotation ``yaw``.
+    def render_semantic_map(self, center_world: np.ndarray, world_to_image_space: np.ndarray) -> np.ndarray:
+        """Renders the semantic map at given x,y coordinates.
 
         Args:
-            x (float): X coordinate to center crop from.
-            y (float): Y coordinate to center crop from.
-            yaw (float): Rotation of the cropped image.
-
+            center_world (np.ndarray): XY of the image center in world ref system
+            world_to_image_space (np.ndarray):
         Returns:
             np.ndarray: RGB raster
 
         """
 
         img = 255 * np.ones(shape=(self.raster_size[1], self.raster_size[0], 3), dtype=np.uint8)
-        r = float(np.linalg.norm(self.raster_size * self.pixel_size))
+
+        # filter using half a radius from the center
+        raster_radius = float(np.linalg.norm(self.raster_size * self.pixel_size)) / 2
 
         # plot lanes
-        lines = []
-        for idx in elements_within_radius(x, y, self.semantic_map["lanes_bounds"], r):
+        lanes_lines = []
+
+        for idx in elements_within_bounds(center_world, self.semantic_map["lanes_bounds"], raster_radius):
             lane = self.semantic_map["lanes"][idx]
 
-            x1, y1 = map_to_image(
-                x, y, yaw, lane["xyz_left"][0], lane["xyz_left"][1], self.raster_size, self.pixel_size
-            )
-            x2, y2 = map_to_image(
-                x, y, yaw, lane["xyz_right"][0], lane["xyz_right"][1], self.raster_size, self.pixel_size
-            )
-            pts1 = np.vstack((x1, y1)).T
-            pts2 = np.vstack((x2, y2)).T
-            lines.append(pts1)
-            lines.append(pts2)
-            poly = np.vstack((pts1, np.flip(pts2, 0)))
-            cv2.fillPoly(img, [poly], (17, 17, 31), lineType=cv2.LINE_AA, shift=8)
+            # get image coords
+            xy_left = cv2_subpixel(transform_points(lane["xyz_left"][:, :2], world_to_image_space))
+            xy_right = cv2_subpixel(transform_points(lane["xyz_right"][:, :2], world_to_image_space))
 
-        cv2.polylines(img, lines, False, (255, 217, 82), lineType=cv2.LINE_AA, shift=8)
+            lanes_area = np.vstack((xy_left, np.flip(xy_right, 0)))  # start->end left then end->start right
+
+            # Note(lberg): this called on all polygons skips some of them, don't know why
+            cv2.fillPoly(img, [lanes_area], (17, 17, 31), lineType=cv2.LINE_AA, shift=CV2_SHIFT)
+
+            lanes_lines.append(xy_left)
+            lanes_lines.append(xy_right)
+
+        cv2.polylines(img, lanes_lines, False, (255, 217, 82), lineType=cv2.LINE_AA, shift=CV2_SHIFT)
 
         # plot crosswalks
         crosswalks = []
-        for idx in elements_within_radius(x, y, self.semantic_map["crosswalks_bounds"], r):
+        for idx in elements_within_bounds(center_world, self.semantic_map["crosswalks_bounds"], raster_radius):
             crosswalk = self.semantic_map["crosswalks"][idx]
-            x1, y1 = map_to_image(
-                x, y, yaw, crosswalk["xyz"][0], crosswalk["xyz"][1], self.raster_size, self.pixel_size
-            )
-            pts = np.vstack((x1, y1)).T
-            crosswalks.append(pts)
+            xy_cross = cv2_subpixel(transform_points(crosswalk["xyz"][:, :2], world_to_image_space))
+            crosswalks.append(xy_cross)
 
-        cv2.polylines(img, crosswalks, True, (255, 117, 69), lineType=cv2.LINE_AA, shift=8)
+        cv2.polylines(img, crosswalks, True, (255, 117, 69), lineType=cv2.LINE_AA, shift=CV2_SHIFT)
 
         return img
 
