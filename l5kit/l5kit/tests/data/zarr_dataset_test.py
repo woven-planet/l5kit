1de465ad83d312bd1b86f17234a7e75f30082ff5
lucabergamini <lucabergamini@lyft.com>
2020-08-13 20:13:07 +0200

    refactor and add util (#97)
    * refactor and add util
    
    * replace single_scene.zarr
    
    * enable repeated paths
    
    * add test for concat util
    
    * address TODO
    
    * add test for util
    
    * replace zarr test
    
    * skip broken zarrs in cat
    
    * add exception handling
    
    * add debug tqdm
    
    * move tqdm
    
    * fix a bug in append with multiple scenes in input
    
    * refactor split utils and add tests
    
    * address review
    
    * fix a bug and add tests

--- l5kit/l5kit/tests/data/zarr_dataset_test.py
+++ l5kit/l5kit/tests/data/zarr_dataset_test.py
@@ -1,27 +1,17 @@
 import numpy as np
-import numpy.testing
 
 from l5kit.data import ChunkedDataset
 
 
 def test_load_dataset(zarr_dataset: ChunkedDataset) -> None:
 
-    assert len(zarr_dataset.frames) == 775
-    assert len(zarr_dataset.agents) == 67954
-    assert len(zarr_dataset.scenes) == 1
+    assert len(zarr_dataset.frames) > 0
+    assert len(zarr_dataset.agents) > 0
+    assert len(zarr_dataset.scenes) > 0
+    assert len(zarr_dataset.tl_faces) > 0
 
-    frame = zarr_dataset.frames[0]
+    # check first and last frames timestamps differ
+    assert zarr_dataset.frames[0]["timestamp"] != zarr_dataset.frames[-1]["timestamp"]
 
-    assert frame["timestamp"] == 1266597039003039366
-    numpy.testing.assert_allclose(frame["agent_index_interval"], (0, 88))
-    numpy.testing.assert_allclose(
-        frame["ego_translation"], np.array([542.73755, -2405.4773, 288.671], dtype=np.float32)
-    )
-    numpy.testing.assert_allclose(
-        frame["ego_rotation"],
-        np.array(
-            [[-0.432687, -0.901447, 0.013263], [0.901538, -0.432583, 0.010026], [-0.003301, 0.016295, 0.999862]],
-            dtype=np.float32,
-        ),
-        rtol=1e-04,
-    )
+    # check positions differ
+    assert np.any(zarr_dataset.frames[0]["ego_translation"] != zarr_dataset.frames[1]["ego_translation"])
