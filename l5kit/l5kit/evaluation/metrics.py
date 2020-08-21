60379a3c9b35f48a0ceeb1379a7d4a781e25c9c2
lucabergamini <lucabergamini@lyft.com>
2020-08-21 17:22:27 +0200

    rename variable

--- l5kit/l5kit/evaluation/metrics.py
+++ l5kit/l5kit/evaluation/metrics.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 
-def _assert_shapes(gt: np.ndarray, pred: np.ndarray, confidences: np.ndarray, avails: np.ndarray) -> None:
+def _assert_shapes(truth: np.ndarray, pred: np.ndarray, confidences: np.ndarray, avails: np.ndarray) -> None:
     """
     Check the shapes of args required by metrics
 
     Args:
-        gt (np.ndarray): array of shape (timesteps)x(2D coords)
+        truth (np.ndarray): array of shape (timesteps)x(2D coords)
         pred (np.ndarray): array of shape (modes)x(timesteps)x(2D coords)
         confidences (np.ndarray): array of shape (modes) with a confidence for each mode in each sample
         avails (np.ndarray): array of shape (timesteps) with the availability for each gt timesteps
@@ -17,19 +17,19 @@ def _assert_shapes(gt: np.ndarray, pred: np.ndarray, confidences: np.ndarray, av
     assert len(pred.shape) == 3, f"expected 3D (MxTxC) array for pred, got {pred.shape}"
     num_modes, future_len, num_coords = pred.shape
 
-    assert gt.shape == (future_len, num_coords), f"expected 2D (Time x Coords) array for gt, got {gt.shape}"
+    assert truth.shape == (future_len, num_coords), f"expected 2D (Time x Coords) array for gt, got {truth.shape}"
     assert confidences.shape == (num_modes,), f"expected 1D (Modes) array for gt, got {confidences.shape}"
     assert np.allclose(np.sum(confidences), 1), "confidences should sum to 1"
     assert avails.shape == (future_len,), f"expected 1D (Time) array for gt, got {avails.shape}"
     # assert all data are valid
     assert np.isfinite(pred).all(), "invalid value found in pred"
-    assert np.isfinite(gt).all(), "invalid value found in gt"
+    assert np.isfinite(truth).all(), "invalid value found in gt"
     assert np.isfinite(confidences).all(), "invalid value found in confidences"
     assert np.isfinite(avails).all(), "invalid value found in avails"
 
 
 def neg_multi_log_likelihood(
-    gt: np.ndarray, pred: np.ndarray, confidences: np.ndarray, avails: np.ndarray
+    truth: np.ndarray, pred: np.ndarray, confidences: np.ndarray, avails: np.ndarray
 ) -> np.ndarray:
     """
     Compute a negative log-likelihood for the multi-modal scenario.
@@ -39,7 +39,7 @@ def neg_multi_log_likelihood(
     https://leimao.github.io/blog/LogSumExp/
 
     Args:
-        gt (np.ndarray): array of shape (timesteps)x(2D coords)
+        truth (np.ndarray): array of shape (timesteps)x(2D coords)
         pred (np.ndarray): array of shape (modes)x(timesteps)x(2D coords)
         confidences (np.ndarray): array of shape (modes) with a confidence for each mode in each sample
         avails (np.ndarray): array of shape (timesteps) with the availability for each gt timesteps
@@ -47,12 +47,12 @@ def neg_multi_log_likelihood(
     Returns:
         np.ndarray: negative log-likelihood for this example, a single float number
     """
-    _assert_shapes(gt, pred, confidences, avails)
+    _assert_shapes(truth, pred, confidences, avails)
 
-    gt = np.expand_dims(gt, 0)  # add modes
+    truth = np.expand_dims(truth, 0)  # add modes
     avails = avails[np.newaxis, :, np.newaxis]  # add modes and cords
 
-    error = np.sum(((gt - pred) * avails) ** 2, axis=-1)  # reduce coords and use availability
+    error = np.sum(((truth - pred) * avails) ** 2, axis=-1)  # reduce coords and use availability
 
     with np.errstate(divide="ignore"):  # when confidence is 0 log goes to -inf, but we're fine with it
         error = np.log(confidences) - 0.5 * np.sum(error, axis=-1)  # reduce timesteps
@@ -63,12 +63,12 @@ def neg_multi_log_likelihood(
     return error
 
 
-def rmse(gt: np.ndarray, pred: np.ndarray, confidences: np.ndarray, avails: np.ndarray) -> np.ndarray:
+def rmse(truth: np.ndarray, pred: np.ndarray, confidences: np.ndarray, avails: np.ndarray) -> np.ndarray:
     """
     Return the root mean squared error, computed using the stable nll
 
     Args:
-        gt (np.ndarray): array of shape (timesteps)x(2D coords)
+        truth (np.ndarray): array of shape (timesteps)x(2D coords)
         pred (np.ndarray): array of shape (modes)x(timesteps)x(2D coords)
         confidences (np.ndarray): array of shape (modes) with a confidence for each mode in each sample
         avails (np.ndarray): array of shape (timesteps) with the availability for each gt timesteps
@@ -77,18 +77,18 @@ def rmse(gt: np.ndarray, pred: np.ndarray, confidences: np.ndarray, avails: np.n
         np.ndarray: negative log-likelihood for this example, a single float number
 
     """
-    nll = neg_multi_log_likelihood(gt, pred, confidences, avails)
+    nll = neg_multi_log_likelihood(truth, pred, confidences, avails)
     _, future_len, _ = pred.shape
 
     return np.sqrt(2 * nll / future_len)
 
 
-def prob_true_mode(gt: np.ndarray, pred: np.ndarray, confidences: np.ndarray, avails: np.ndarray) -> np.ndarray:
+def prob_true_mode(truth: np.ndarray, pred: np.ndarray, confidences: np.ndarray, avails: np.ndarray) -> np.ndarray:
     """
     Return the probability of the true mode
 
     Args:
-        gt (np.ndarray): array of shape (timesteps)x(2D coords)
+        truth (np.ndarray): array of shape (timesteps)x(2D coords)
         pred (np.ndarray): array of shape (modes)x(timesteps)x(2D coords)
         confidences (np.ndarray): array of shape (modes) with a confidence for each mode in each sample
         avails (np.ndarray): array of shape (timesteps) with the availability for each gt timesteps
@@ -97,12 +97,12 @@ def prob_true_mode(gt: np.ndarray, pred: np.ndarray, confidences: np.ndarray, av
         np.ndarray: a (modes) numpy array
 
     """
-    _assert_shapes(gt, pred, confidences, avails)
+    _assert_shapes(truth, pred, confidences, avails)
 
-    gt = np.expand_dims(gt, 0)  # add modes
+    truth = np.expand_dims(truth, 0)  # add modes
     avails = avails[np.newaxis, :, np.newaxis]  # add modes and cords
 
-    error = np.sum(((gt - pred) * avails) ** 2, axis=-1)  # reduce coords and use availability
+    error = np.sum(((truth - pred) * avails) ** 2, axis=-1)  # reduce coords and use availability
 
     with np.errstate(divide="ignore"):  # when confidence is 0 log goes to -inf, but we're fine with it
         error = np.log(confidences) - 0.5 * np.sum(error, axis=-1)  # reduce timesteps
@@ -114,12 +114,12 @@ def prob_true_mode(gt: np.ndarray, pred: np.ndarray, confidences: np.ndarray, av
     return error
 
 
-def time_displace(gt: np.ndarray, pred: np.ndarray, confidences: np.ndarray, avails: np.ndarray) -> np.ndarray:
+def time_displace(truth: np.ndarray, pred: np.ndarray, confidences: np.ndarray, avails: np.ndarray) -> np.ndarray:
     """
     Return the displacement at timesteps T
 
     Args:
-        gt (np.ndarray): array of shape (timesteps)x(2D coords)
+        truth (np.ndarray): array of shape (timesteps)x(2D coords)
         pred (np.ndarray): array of shape (modes)x(timesteps)x(2D coords)
         confidences (np.ndarray): array of shape (modes) with a confidence for each mode in each sample
         avails (np.ndarray): array of shape (timesteps) with the availability for each gt timesteps
@@ -128,11 +128,11 @@ def time_displace(gt: np.ndarray, pred: np.ndarray, confidences: np.ndarray, ava
         np.ndarray: a (timesteps) numpy array
 
     """
-    true_mode_error = prob_true_mode(gt, pred, confidences, avails)
+    true_mode_error = prob_true_mode(truth, pred, confidences, avails)
     true_mode_error = true_mode_error[:, None]  # add timesteps axis
 
-    gt = np.expand_dims(gt, 0)  # add modes
+    truth = np.expand_dims(truth, 0)  # add modes
     avails = avails[np.newaxis, :, np.newaxis]  # add modes and cords
 
-    error = np.sum(((gt - pred) * avails) ** 2, axis=-1)  # reduce coords and use availability
+    error = np.sum(((truth - pred) * avails) ** 2, axis=-1)  # reduce coords and use availability
     return np.sum(true_mode_error * np.sqrt(error), axis=0)  # reduce modes
