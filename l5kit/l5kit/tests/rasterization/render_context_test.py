7f212a4ad430750b623d0803cba6097c525ba97b
lucabergamini <lucabergamini@lyft.com>
2020-12-08 11:29:06 +0100

    add RenderContext test

--- l5kit/l5kit/tests/rasterization/render_context_test.py
+++ l5kit/l5kit/tests/rasterization/render_context_test.py
@@ -1,10 +1,12 @@
 import numpy as np
+import pytest
 
 from l5kit.geometry import transform_points
 from l5kit.rasterization.render_context import RenderContext
 
 
-def test_transform_points_to_raster() -> None:
+@pytest.mark.parametrize("set_origin_to_bottom", [False, True])
+def test_transform_points_to_raster(set_origin_to_bottom: bool) -> None:
     image_shape_px = np.asarray((200, 200))
     center_in_raster_ratio = np.asarray((0.5, 0.5))
     pixel_size_m = np.asarray((1.0, 1.0))
@@ -14,11 +16,14 @@ def test_transform_points_to_raster() -> None:
         raster_size_px=image_shape_px,
         pixel_size_m=pixel_size_m,
         center_in_raster_ratio=center_in_raster_ratio,
-        set_origin_to_bottom=False,
+        set_origin_to_bottom=set_origin_to_bottom,
     )
 
     input_points = np.array([[0, 0], [10, 10], [-10, -10]])
-    expected_output_points = np.array([[100, 102], [110, 112], [90, 92]])
+    if not set_origin_to_bottom:
+        expected_output_points = np.array([[100, 102], [110, 112], [90, 92]])
+    else:
+        expected_output_points = np.array([[100, 98], [110, 88], [90, 108]])
 
     tf = render_context.raster_from_world(center_world, 0.0)
     output_points = transform_points(input_points, tf)
