82fe0e89d5441d72d1938122335370ef6521931c
Pascal Pfeiffer <1069138+pascal-pfeiffer@users.noreply.github.com>
2021-01-13 11:30:08 +0100

    [Fix] Satellite Image Transform for Non-Squared Raster (#221)
    * fix satellite image
    
    non squared raster sizes caused wrong transforms
    
    * added test for rectangular satellite image
    
    * assert crop_im.shape

--- l5kit/l5kit/tests/rasterization/satellite_image_test.py
+++ l5kit/l5kit/tests/rasterization/satellite_image_test.py
@@ -5,7 +5,7 @@ import pytest
 from l5kit.rasterization import get_sat_image_crop, get_sat_image_crop_scaled
 
 
-def test_satellite_image_cropping() -> None:
+def test_satellite_image_cropping_square() -> None:
 
     sat_image = np.zeros((1001, 1001, 3), dtype=np.float32)
     sat_image[500:, :500, 0] = 1  # Make top right corner red
@@ -17,6 +17,29 @@ def test_satellite_image_cropping() -> None:
             sat_image, crop_size=(200, 200), sat_pixel_translation=np.array([500, 500]), yaw=yaw
         )
         assert crop_im.shape == (200, 200, 3)
+
+        assert crop_im[..., 0].mean() == pytest.approx(0.25, 0.1)  # One quarter should still be red
+        assert crop_im[..., 1].mean() == pytest.approx(0.25, 0.1)  # One quarter should still be green
+        if yaw == 0.5 * np.pi:
+            # The top left should be blue now
+            np.testing.assert_array_equal(crop_im[0, 0], np.array([0, 0, 1]))
+
+    with pytest.raises(IndexError):
+        get_sat_image_crop_scaled(sat_image, crop_size=(10, 10), sat_pixel_translation=np.array([0, 1000]))
+
+
+def test_satellite_image_cropping_rectangular() -> None:
+
+    sat_image = np.zeros((1001, 1001, 3), dtype=np.float32)
+    sat_image[500:, :500, 0] = 1  # Make top right corner red
+    sat_image[:500, :500, 1] = 1  # Make bottom right corner green
+    sat_image[:500, 500:, 2] = 1  # Make bottom left corner blue
+
+    for yaw in [None, np.pi, 0.5 * np.pi]:
+        crop_im = get_sat_image_crop_scaled(
+            sat_image, crop_size=(200, 100), sat_pixel_translation=np.array([500, 500]), yaw=yaw
+        )
+        assert crop_im.shape == (100, 200, 3)
 
         assert crop_im[..., 0].mean() == pytest.approx(0.25, 0.1)  # One quarter should still be red
         assert crop_im[..., 1].mean() == pytest.approx(0.25, 0.1)  # One quarter should still be green
