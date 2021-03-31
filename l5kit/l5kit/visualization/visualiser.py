67955b81b93b9c783a8d844bab95c489629eb32b
lucabergamini <lucabergamini@lyft.com>
2021-03-31 10:46:04 +0200

    factor out trajectories, use new util

--- l5kit/l5kit/visualization/visualiser.py
+++ l5kit/l5kit/visualization/visualiser.py
@@ -1,32 +1,25 @@
+from collections import defaultdict
+from typing import DefaultDict, List, Tuple
+
 import bokeh.io
 import bokeh.plotting
 import numpy as np
 from bokeh.layouts import column
-from bokeh.models import CustomJS, HoverTool, Slider, CheckboxGroup
+from bokeh.models import CustomJS, HoverTool, Slider
 from bokeh.plotting import ColumnDataSource, output_file, save
 
 from l5kit.configs import load_config_data
-from l5kit.data import (
-    ChunkedDataset,
-    LocalDataManager,
-    get_agents_slice_from_frames,
-    get_frames_slice_from_scenes,
-    get_tl_faces_slice_from_frames,
-)
-from l5kit.data.filter import (
-    filter_agents_by_frames,
-    filter_agents_by_labels,
-    filter_tl_faces_by_frames,
-    filter_tl_faces_by_status,
-)
+from l5kit.data import ChunkedDataset, LocalDataManager
+from l5kit.data.filter import (filter_agents_by_frames, filter_agents_by_labels, filter_tl_faces_by_frames,
+                               filter_tl_faces_by_status)
 from l5kit.data.labels import PERCEPTION_LABELS
 from l5kit.data.map_api import MapAPI, TLFacesColors
 from l5kit.geometry import compute_agent_pose, rotation33_as_yaw, transform_points
 from l5kit.rasterization import build_rasterizer
 from l5kit.rasterization.box_rasterizer import get_ego_as_agent
 from l5kit.rasterization.semantic_rasterizer import indices_in_bounds
-from collections import defaultdict
 from l5kit.sampling import get_relative_poses
+
 
 COLORS = {
     TLFacesColors.GREEN.name: "#33CC33",
@@ -36,6 +29,36 @@ COLORS = {
     "PERCEPTION_LABEL_CYCLIST": "#CC33FF",
     "PERCEPTION_LABEL_PEDESTRIAN": "#66CCFF",
 }
+
+
+def get_frame_trajectories(frames: np.ndarray, agents_frames: List[np.ndarray], track_ids: np.ndarray,
+                           frame_index: int) -> Tuple[DefaultDict, DefaultDict]:
+
+    ego_xy = frames[frame_index]["ego_translation"][:2]
+    ego_yaw = rotation33_as_yaw(frames[frame_index]["ego_rotation"])
+    world_from_agent = compute_agent_pose(ego_xy, ego_yaw)
+    agent_from_world = np.linalg.inv(world_from_agent)
+
+    traj_agents = defaultdict(list)
+    # TODO: factor out future length
+    agent_traj_length = 20
+    for track_id in track_ids:
+        pos, *_, avail = get_relative_poses(agent_traj_length, frames[frame_index: frame_index + agent_traj_length],
+                                            track_id, agents_frames[frame_index: frame_index + agent_traj_length],
+                                            agent_from_world, ego_yaw)
+        traj_agents["x"].append(pos[avail > 0, 0])
+        traj_agents["y"].append(pos[avail > 0, 1])
+
+    traj_ego = defaultdict(list)
+    # TODO: factor out future length
+    ego_traj_length = 100
+    pos, *_, avail = get_relative_poses(ego_traj_length, frames[frame_index: frame_index + ego_traj_length],
+                                        None, agents_frames[frame_index: frame_index + ego_traj_length],
+                                        agent_from_world, ego_yaw)
+    traj_ego["x"].append(pos[avail > 0, 0])
+    traj_ego["y"].append(pos[avail > 0, 1])
+
+    return traj_agents, traj_ego
 
 
 def visualise_scene(zarr_dataset: ChunkedDataset, scene_index: int, mapAPI: MapAPI):
@@ -62,21 +85,11 @@ def visualise_scene(zarr_dataset: ChunkedDataset, scene_index: int, mapAPI: MapA
     f.xgrid.grid_line_color = None
     f.ygrid.grid_line_color = None
 
-    # TODO: move this into a function (zarr util)
-    scenes = zarr_dataset.scenes[scene_index: scene_index + 1].copy()
-
-    frame_slice = get_frames_slice_from_scenes(*scenes)
-    frames = zarr_dataset.frames[frame_slice].copy()
-
-    agent_slice = get_agents_slice_from_frames(*frames[[0, -1]])
-    agents = zarr_dataset.agents[agent_slice].copy()
-
-    tl_slice = get_tl_faces_slice_from_frames(*frames[[0, -1]])
-    tl_faces = zarr_dataset.tl_faces[tl_slice].copy()
-
-    frames["agent_index_interval"] -= agent_slice.start
-    frames["traffic_light_faces_index_interval"] -= tl_slice.start
-    scenes["frame_index_interval"] -= frame_slice.start
+    # get a new dataset with only that scene
+    scene_dataset = zarr_dataset.get_scene_dataset(scene_index)
+    frames = scene_dataset.frames
+    agents = scene_dataset.agents
+    tl_faces = scene_dataset.tl_faces
 
     agents_frames = filter_agents_by_frames(frames, agents)
     tls_frames = filter_tl_faces_by_frames(frames, tl_faces)
@@ -85,36 +98,14 @@ def visualise_scene(zarr_dataset: ChunkedDataset, scene_index: int, mapAPI: MapA
     for frame_idx in range(len(frames)):
         frame = frames[frame_idx]
         agents_frame = agents_frames[frame_idx]
+
+        # TODO: hardcoded threshold
+        agents_frame = filter_agents_by_labels(agents_frame, 0.1)
+
         tls_frame = tls_frames[frame_idx]
 
         lanes, crosswalks, ego, agent = get_frame_data(mapAPI, frame, agents_frame, tls_frame)
-
-        # trajectories
-        # TODO: make function
-        # TODO: repeated
-        ego_xy = frame["ego_translation"][:2]
-        ego_yaw = rotation33_as_yaw(frame["ego_rotation"])
-        world_from_agent = compute_agent_pose(ego_xy, ego_yaw)
-        agent_from_world = np.linalg.inv(world_from_agent)
-
-        trajs = defaultdict(list)
-        # TODO: factor out future length
-        agent_traj_length = 20
-        for track_id in agent["id"]:
-            pos, *_, avail = get_relative_poses(agent_traj_length, frames[frame_idx: frame_idx + agent_traj_length],
-                                                track_id, agents_frames[frame_idx: frame_idx + agent_traj_length],
-                                                agent_from_world, ego_yaw)
-            trajs["x"].append(pos[avail > 0, 0])
-            trajs["y"].append(pos[avail > 0, 1])
-
-        traj_ego = defaultdict(list)
-        # TODO: factor out future length
-        ego_traj_length = 100
-        pos, *_, avail = get_relative_poses(ego_traj_length, frames[frame_idx: frame_idx + ego_traj_length],
-                                            None, agents_frames[frame_idx: frame_idx + ego_traj_length],
-                                            agent_from_world, ego_yaw)
-        traj_ego["x"].append(pos[avail > 0, 0])
-        traj_ego["y"].append(pos[avail > 0, 1])
+        trajs, traj_ego = get_frame_trajectories(frames, agents_frames, agents_frame["track_id"], frame_idx)
 
         trajs = ColumnDataSource(data=trajs)
         traj_ego = ColumnDataSource(data=traj_ego)
@@ -215,13 +206,10 @@ def get_frame_data(mapAPI: MapAPI, frame: np.ndarray, agents: np.ndarray, tls_fr
     ego_dict = dict(x=[], y=[])
     agents_dict = defaultdict(list)
 
-    agents = filter_agents_by_labels(agents, 0.1)
     agents = np.insert(agents, 0, get_ego_as_agent(frame))
 
     # TODO: move to a function
-    corners_base_coords = (np.asarray([[-1, -1], [-1, 1], [1, 1], [1, -1]]) * 0.5)[
-                          None, :, :
-                          ]
+    corners_base_coords = (np.asarray([[-1, -1], [-1, 1], [1, 1], [1, -1]]) * 0.5)[None, :, :]
     corners_m = corners_base_coords * agents["extent"][:, None, :2]  # corners in zero
     s = np.sin(agents["yaw"])
     c = np.cos(agents["yaw"])
