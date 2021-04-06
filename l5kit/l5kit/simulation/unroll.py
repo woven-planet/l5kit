9517b4c89f776cd3d8c09e50435df77567ad12b5
lucabergamini <lucabergamini@lyft.com>
2021-04-06 11:12:12 +0100

    Simulation is now an object, fix tests

--- l5kit/l5kit/simulation/unroll.py
+++ l5kit/l5kit/simulation/unroll.py
@@ -1,104 +1,125 @@
-from typing import Any, Dict, List, Optional, Tuple
+from typing import Dict, List, Optional, Tuple, NamedTuple
 
-import cv2
 import numpy as np
 import torch
-from torch.utils.data import DataLoader
 from tqdm.auto import tqdm
-from l5kit.geometry import transform_points, transform_point
-from l5kit.rasterization import Rasterizer
-from l5kit.rasterization.box_rasterizer import draw_boxes, get_ego_as_agent
-from l5kit.visualization import PREDICTED_POINTS_COLOR, TARGET_POINTS_COLOR, draw_trajectory
+from l5kit.geometry import transform_points
 from l5kit.data import AGENT_DTYPE
-from torch.utils.data.dataloader import default_collate
-from collections import defaultdict
 
+from l5kit.dataset import EgoDataset
 from l5kit.simulation.dataset import SimulationDataset
 from torch.utils.data.dataloader import default_collate
 
 
-def update_agents(dataset: SimulationDataset, frame_idx: int, input_dict: Dict[str, torch.Tensor],
-                  output_dict: Dict[str, torch.Tensor]) -> None:
-    """Update the agents in frame_idx (across scenes) using agents_output_dict
-
-    :param dataset: the simulation dataset
-    :param frame_idx: index of the frame to modify
-    :param input_dict: the input to the agent model
-    :param output_dict: the output of the agent model
-    :return:
-    """
-
-    agents_update_dict: Dict[Tuple[int, int]: np.ndarray] = {}
-    input_dict = {k: v.cpu().numpy() for k, v in input_dict.items()}
-    output_dict = {k: v.cpu().numpy() for k, v in output_dict.items()}
-
-    world_from_agent = input_dict["world_from_agent"]
-    yaw = input_dict["yaw"]
-    pred_trs = transform_points(output_dict["positions"][:, :1], world_from_agent)[:, 0]
-    pred_yaws = yaw + output_dict["yaws"][:, 0]
-
-    next_agents = np.zeros(len(yaw), dtype=AGENT_DTYPE)
-    next_agents["centroid"] = pred_trs
-    next_agents["yaw"] = pred_yaws
-    next_agents["track_id"] = input_dict["track_id"]
-    next_agents["extent"] = input_dict["extent"]
-    next_agents["label_probabilities"][:, 3] = 1
-
-    for scene_idx, next_agent in zip(input_dict["scene_index"], next_agents):
-        agents_update_dict[(scene_idx, next_agent["track_id"])] = np.expand_dims(next_agent, 0)
-    dataset.set_agents(frame_idx, agents_update_dict)
+class SimulationConfig(NamedTuple):
+    use_ego_gt: bool
+    use_agents_gt: bool
+    disable_new_agents: bool
+    distance_th_far: float
+    distance_th_close: float
+    start_frame_index: int = 0
+    num_simulation_steps: Optional[int] = None
 
 
-def update_ego(dataset: SimulationDataset, frame_idx: int, input_dict: Dict[str, torch.Tensor],
-                  output_dict: Dict[str, torch.Tensor]) -> None:
-    """Update ego across scenes for the given frame index.
+class SimulationLoop:
+    def __init__(self, sim_cfg: SimulationConfig, model_ego: torch.nn.Module, model_agents: torch.nn.Module):
+        self.sim_cfg = sim_cfg
+        self.model_ego = model_ego
+        self.model_agents = model_agents
 
-    :param dataset: The simulation dataset
-    :param frame_idx: index of the frame to modify
-    :param input_dict: the input to the ego model
-    :param output_dict: the output of the ego model
-    :return:
-    """
-    input_dict = {k: v.cpu().numpy() for k, v in input_dict.items()}
-    output_dict = {k: v.cpu().numpy() for k, v in output_dict.items()}
+    def unroll(self, dataset: EgoDataset, scene_indices: List[int]) -> SimulationDataset:
+        """
+        Simulate the given dataset for the given scene indices
+        :param dataset: the EgoDataset
+        :param scene_indices: the scene indices we want to simulate
+        :return: the simulated dataset
+        TODO: this should probably return something else
+        """
+        sim_dataset = SimulationDataset(dataset, scene_indices, self.sim_cfg.start_frame_index,
+                                        self.sim_cfg.disable_new_agents, self.sim_cfg.distance_th_far,
+                                        self.sim_cfg.distance_th_close)
 
-    world_from_agent = input_dict["world_from_agent"]
-    yaw = input_dict["yaw"]
-    pred_trs = transform_points(output_dict["positions"][:, :1], world_from_agent)
-    pred_yaws = yaw + output_dict["yaws"][:, :1]
+        if self.sim_cfg.num_simulation_steps is None:
+            range_unroll = range(self.sim_cfg.start_frame_index, len(sim_dataset))
+        else:
+            end_frame_index = min(len(sim_dataset), self.sim_cfg.num_simulation_steps + self.sim_cfg.start_frame_index)
+            range_unroll = range(self.sim_cfg.start_frame_index, end_frame_index)
 
-    dataset.set_ego_for_frame(frame_idx, 0, pred_trs, pred_yaws)
+        for frame_index in tqdm(range_unroll):
+            next_frame_index = frame_index + 1
 
+            # AGENTS
+            agents_input = sim_dataset.rasterise_agents_frame_batch(frame_index)
+            agents_input_dict = default_collate(list(agents_input.values()))
+            agents_output_dict = self.model_agents(agents_input_dict)
 
-@torch.no_grad()
-def unroll(
-    cfg: Dict,
-    model_ego: torch.nn.Module,
-    model_agents: torch.nn.Module,
-    dataset: SimulationDataset,
-    start_frame_index: int = 0,
-    end_frame_index: Optional[int] = None,
-    ) -> None:
+            # EGO
+            ego_input = sim_dataset.rasterise_frame_batch(frame_index)
+            ego_input_dict = default_collate(ego_input)
+            ego_output_dict = self.model_ego(ego_input_dict)
 
-    if end_frame_index is None:
-        end_frame_index = len(dataset)
+            if next_frame_index != range_unroll.stop:
+                if not self.sim_cfg.use_agents_gt:
+                    # TODO: suboptimal as we're still doing forward
+                    self.update_agents(sim_dataset, next_frame_index, agents_input_dict, agents_output_dict)
+                if not self.sim_cfg.use_ego_gt:
+                    # TODO: suboptimal as we're still doing forward
+                    self.update_ego(sim_dataset, next_frame_index, ego_input_dict, ego_output_dict)
 
-    for frame_index in tqdm(range(start_frame_index, end_frame_index)):
-        next_frame_index = frame_index + 1
+        return sim_dataset
 
-        # AGENTS
-        agents_input = dataset.rasterise_agents_frame_batch(frame_index)
-        agents_input_dict = default_collate(list(agents_input.values()))
-        agents_output_dict = model_agents(agents_input_dict)
+    @staticmethod
+    def update_agents(dataset: SimulationDataset, frame_idx: int, input_dict: Dict[str, torch.Tensor],
+                      output_dict: Dict[str, torch.Tensor]) -> None:
+        """Update the agents in frame_idx (across scenes) using agents_output_dict
 
-        # EGO
-        ego_input = dataset.rasterise_frame_batch(frame_index)
-        ego_input_dict = default_collate(ego_input)
-        ego_output_dict = model_ego(ego_input_dict)
+        :param dataset: the simulation dataset
+        :param frame_idx: index of the frame to modify
+        :param input_dict: the input to the agent model
+        :param output_dict: the output of the agent model
+        :return:
+        """
 
-        if next_frame_index != end_frame_index:
-            update_agents(dataset, next_frame_index, agents_input_dict, agents_output_dict)
-            update_ego(dataset, next_frame_index, ego_input_dict, ego_output_dict)
+        agents_update_dict: Dict[Tuple[int, int]: np.ndarray] = {}
+        input_dict = {k: v.cpu().numpy() for k, v in input_dict.items()}
+        output_dict = {k: v.cpu().numpy() for k, v in output_dict.items()}
+
+        world_from_agent = input_dict["world_from_agent"]
+        yaw = input_dict["yaw"]
+        pred_trs = transform_points(output_dict["positions"][:, :1], world_from_agent)[:, 0]
+        pred_yaws = yaw + output_dict["yaws"][:, 0]
+
+        next_agents = np.zeros(len(yaw), dtype=AGENT_DTYPE)
+        next_agents["centroid"] = pred_trs
+        next_agents["yaw"] = pred_yaws
+        next_agents["track_id"] = input_dict["track_id"]
+        next_agents["extent"] = input_dict["extent"]
+        next_agents["label_probabilities"][:, 3] = 1
+
+        for scene_idx, next_agent in zip(input_dict["scene_index"], next_agents):
+            agents_update_dict[(scene_idx, next_agent["track_id"])] = np.expand_dims(next_agent, 0)
+        dataset.set_agents(frame_idx, agents_update_dict)
+
+    @staticmethod
+    def update_ego(dataset: SimulationDataset, frame_idx: int, input_dict: Dict[str, torch.Tensor],
+                   output_dict: Dict[str, torch.Tensor]) -> None:
+        """Update ego across scenes for the given frame index.
+
+        :param dataset: The simulation dataset
+        :param frame_idx: index of the frame to modify
+        :param input_dict: the input to the ego model
+        :param output_dict: the output of the ego model
+        :return:
+        """
+        input_dict = {k: v.cpu().numpy() for k, v in input_dict.items()}
+        output_dict = {k: v.cpu().numpy() for k, v in output_dict.items()}
+
+        world_from_agent = input_dict["world_from_agent"]
+        yaw = input_dict["yaw"]
+        pred_trs = transform_points(output_dict["positions"][:, :1], world_from_agent)
+        pred_yaws = yaw + output_dict["yaws"][:, :1]
+
+        dataset.set_ego_for_frame(frame_idx, 0, pred_trs, pred_yaws)
 
 
 from l5kit.data import ChunkedDataset, LocalDataManager
@@ -140,6 +161,11 @@ if __name__ == '__main__':
 
     rast = build_rasterizer(cfg, LocalDataManager("/tmp/l5kit_data"))
 
-    dataset = SimulationDataset(EgoDataset(cfg, zarr_dt, rast), [5, 6, 7, 8])
-
-    unroll(cfg, MockModel(), MockModel(), dataset)
+    sim_cfg = SimulationConfig(use_ego_gt=False,
+                               use_agents_gt=False,
+                               disable_new_agents=False,
+                               distance_th_far=10,
+                               distance_th_close=30,
+                               start_frame_index=0)
+    sim = SimulationLoop(sim_cfg, MockModel(), MockModel())
+    sim.unroll(EgoDataset(cfg, zarr_dt, rast), [5, 6, 7, 8])
--- l5kit/l5kit/tests/simulation/unroll_test.py
+++ l5kit/l5kit/tests/simulation/unroll_test.py
@@ -6,8 +6,7 @@ import torch
 from l5kit.data import ChunkedDataset, filter_agents_by_track_id, LocalDataManager
 from l5kit.dataset import EgoDataset
 from l5kit.rasterization import build_rasterizer
-from l5kit.simulation.dataset import SimulationDataset
-from l5kit.simulation.unroll import unroll
+from l5kit.simulation.unroll import SimulationConfig, SimulationLoop
 
 
 class MockModel(torch.nn.Module):
@@ -34,8 +33,8 @@ def test_unroll(zarr_cat_dataset: ChunkedDataset, dmg: LocalDataManager, cfg: di
     ego_dataset = EgoDataset(cfg, zarr_cat_dataset, rasterizer)
 
     # control only agents at T0, control them forever
-    dataset = SimulationDataset(ego_dataset, scene_indices, distance_th_close=1000,
-                                distance_th_far=1000, disable_new_agents=True)
+    sim_cfg = SimulationConfig(use_ego_gt=False, use_agents_gt=False, disable_new_agents=True,
+                               distance_th_close=1000, distance_th_far=1000, num_simulation_steps=10)
 
     # ego will move by 1 each time
     ego_model = MockModel(advance_x=1.0)
@@ -43,20 +42,19 @@ def test_unroll(zarr_cat_dataset: ChunkedDataset, dmg: LocalDataManager, cfg: di
     # agents will move by 0.5 each time
     agents_model = MockModel(advance_x=0.5)
 
-    simulation_steps = 10
-    unroll(cfg, ego_model, agents_model, dataset, end_frame_index=simulation_steps)
+    sim = SimulationLoop(sim_cfg, ego_model, agents_model)
+    sim_dataset = sim.unroll(ego_dataset, scene_indices)
 
-    # TODO: change this when unroll has a return
     # check ego movement
-    for dt_simulated in dataset.scene_dataset_batch.values():
-        ego_tr = dt_simulated.dataset.frames["ego_translation"][: simulation_steps, :2]
+    for dt_simulated in sim_dataset.scene_dataset_batch.values():
+        ego_tr = dt_simulated.dataset.frames["ego_translation"][: sim_cfg.num_simulation_steps, :2]
         ego_dist = np.linalg.norm(np.diff(ego_tr, axis=0), axis=-1)
         assert np.allclose(ego_dist, 1.0)
 
     # check agents movements
-    agents_tracks = [el[1] for el in dataset.agents_tracked]
-    for dt_simulated in dataset.scene_dataset_batch.values():
+    agents_tracks = [el[1] for el in sim_dataset.agents_tracked]
+    for dt_simulated in sim_dataset.scene_dataset_batch.values():
         for track_id in agents_tracks:
-            agents = filter_agents_by_track_id(dt_simulated.dataset.agents, track_id)[: simulation_steps]
+            agents = filter_agents_by_track_id(dt_simulated.dataset.agents, track_id)[: sim_cfg.num_simulation_steps]
             agent_dist = np.linalg.norm(np.diff(agents["centroid"], axis=0), axis=-1)
             assert np.allclose(agent_dist, 0.5)
