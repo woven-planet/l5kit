3852d2b5d11abb0a1420d1b7ccc0dfd86e725965
lucabergamini <lucabergamini@lyft.com>
2021-04-07 11:21:48 +0100

    add back trajectories

--- l5kit/l5kit/visualization/visualiser.py
+++ l5kit/l5kit/visualization/visualiser.py
@@ -1,5 +1,4 @@
-from collections import defaultdict
-from typing import DefaultDict, List, Tuple, NamedTuple, Dict
+from typing import List, NamedTuple, Dict
 
 import bokeh.io
 import bokeh.plotting
@@ -14,7 +13,6 @@ from l5kit.data.filter import (filter_agents_by_frames, filter_agents_by_labels,
                                filter_tl_faces_by_status)
 from l5kit.data.labels import PERCEPTION_LABELS
 from l5kit.data.map_api import MapAPI, TLFacesColors
-from l5kit.geometry import compute_agent_pose, rotation33_as_yaw, transform_points
 from l5kit.rasterization import build_rasterizer
 from l5kit.rasterization.box_rasterizer import get_ego_as_agent
 from l5kit.rasterization.semantic_rasterizer import indices_in_bounds
@@ -29,31 +27,6 @@ COLORS = {
     "PERCEPTION_LABEL_CYCLIST": "#CC33FF",
     "PERCEPTION_LABEL_PEDESTRIAN": "#66CCFF",
 }
-
-
-def get_frame_trajectories(frames: np.ndarray, agents_frames: List[np.ndarray], track_ids: np.ndarray,
-                           frame_index: int) -> Tuple[DefaultDict, DefaultDict]:
-
-    traj_agents = defaultdict(list)
-    # TODO: factor out future length
-    agent_traj_length = 20
-    for track_id in track_ids:
-        pos, *_, avail = get_relative_poses(agent_traj_length, frames[frame_index: frame_index + agent_traj_length],
-                                            track_id, agents_frames[frame_index: frame_index + agent_traj_length],
-                                            np.eye(3), 0)
-        traj_agents["x"].append(pos[avail > 0, 0])
-        traj_agents["y"].append(pos[avail > 0, 1])
-
-    traj_ego = defaultdict(list)
-    # TODO: factor out future length
-    ego_traj_length = 100
-    pos, *_, avail = get_relative_poses(ego_traj_length, frames[frame_index: frame_index + ego_traj_length],
-                                        None, agents_frames[frame_index: frame_index + ego_traj_length],
-                                        np.eye(3), 0)
-    traj_ego["x"].append(pos[avail > 0, 0])
-    traj_ego["y"].append(pos[avail > 0, 1])
-
-    return traj_agents, traj_ego
 
 
 class LaneVisualisation(NamedTuple):
@@ -89,7 +62,7 @@ class TrajectoryVisualisation(NamedTuple):
     xs: np.ndarray
     ys: np.ndarray
     color: str
-    name: str
+    legend_label: str
 
 
 class FrameVisualisation(NamedTuple):
@@ -100,113 +73,55 @@ class FrameVisualisation(NamedTuple):
     trajectories: List[TrajectoryVisualisation]
 
 
-# def visualise_scene(zarr_dataset: ChunkedDataset, scene_index: int, mapAPI: MapAPI):
-#     output_file("scene.html")
-#     agent_hover = HoverTool(
-#         mode="mouse",
-#         names=["agent"],
-#         tooltips=[
-#             ("Type", "@name"),
-#             ("Probability", "@p{0.00}%"),
-#             ("Track id", "@id"),
-#         ],
-#     )
-#
-#     # get a new dataset with only that scene
-#     scene_dataset = zarr_dataset.get_scene_dataset(scene_index)
-#     frames = scene_dataset.frames
-#     agents = scene_dataset.agents
-#     tl_faces = scene_dataset.tl_faces
-#
-#     agents_frames = filter_agents_by_frames(frames, agents)
-#     tls_frames = filter_tl_faces_by_frames(frames, tl_faces)
-#
-#     out = []
-#     for frame_idx in range(len(frames)):
-#         frame = frames[frame_idx]
-#         agents_frame = agents_frames[frame_idx]
-#
-#         # TODO: hardcoded threshold
-#         agents_frame = filter_agents_by_labels(agents_frame, 0.1)
-#
-#         tls_frame = tls_frames[frame_idx]
-#
-#         lanes, crosswalks, ego, agent = get_frame_data(mapAPI, frame, agents_frame, tls_frame)
-#         trajs, traj_ego = get_frame_trajectories(frames, agents_frames, agents_frame["track_id"], frame_idx)
-#
-#         trajs = ColumnDataSource(data=trajs)
-#         traj_ego = ColumnDataSource(data=traj_ego)
-#
-#         lanes = ColumnDataSource(data=lanes)
-#         crosswalks = ColumnDataSource(data=crosswalks)
-#         ego = ColumnDataSource(data=ego)
-#         agent = ColumnDataSource(data=agent)
-#
-#         out.append(dict(lanes=lanes, crosswalks=crosswalks, ego=ego, agent=agent, trajs=trajs, traj_ego=traj_ego))
-#
-#     f = bokeh.plotting.figure(
-#         title="Scene {}".format(scene_index),
-#         match_aspect=True,
-#         x_range=(out[0]["ego"].data["center_x"][0] - 50, out[0]["ego"].data["center_x"][0] + 50),
-#         y_range=(out[0]["ego"].data["center_y"][0] - 50, out[0]["ego"].data["center_y"][0] + 50),
-#         tools=["pan", "wheel_zoom", agent_hover, "save", "reset"],
-#         active_scroll="wheel_zoom",
-#     )
-#
-#     f.xgrid.grid_line_color = None
-#     f.ygrid.grid_line_color = None
-#
-#     f.patches(xs="x", ys="y", color="color", line_width=0, alpha=0.5, source=out[0]["lanes"])
-#     f.patches("x", "y", line_width=0, alpha=0.5, color="#B5B50D", source=out[0]["crosswalks"])
-#     f.patches("x", "y", line_width=2, color="#B53331", source=out[0]["ego"])
-#     f.patches(xs="x", ys="y", color="color", line_width=2, name="agent", source=out[0]["agent"])
-#     f.multi_line("x", "y", alpha=0.8, color="pink", line_width=3, source=out[0]["trajs"], legend_label="trajs")
-#     f.multi_line("x", "y", alpha=0.8, color="red", line_width=3, source=out[0]["traj_ego"], legend_label="traj_ego")
-#
-#     slider_callback = CustomJS(
-#         args=dict(figure=f, sources=out[0], frames=out),
-#         code="""
-#             sources["lanes"].data = frames[cb_obj.value]["lanes"].data;
-#             sources["crosswalks"].data = frames[cb_obj.value]["crosswalks"].data;
-#             sources["agent"].data = frames[cb_obj.value]["agent"].data;
-#             sources["ego"].data = frames[cb_obj.value]["ego"].data;
-#             sources["trajs"].data = frames[cb_obj.value]["trajs"].data;
-#             sources["traj_ego"].data = frames[cb_obj.value]["traj_ego"].data;
-#
-#             figure.x_range.setv({"start": frames[cb_obj.value]["ego"].data["center_x"][0]-50, "end": frames[cb_obj.value]["ego"].data["center_x"][0]+50})
-#             figure.y_range.setv({"start": frames[cb_obj.value]["ego"].data["center_y"][0]-50, "end": frames[cb_obj.value]["ego"].data["center_y"][0]+50})
-#
-#             sources["lanes"].change.emit();
-#             sources["crosswalks"].change.emit();
-#             sources["agent"].change.emit();
-#             sources["ego"].change.emit();
-#             sources["trajs"].change.emit();
-#             sources["traj_ego"].change.emit();
-#         """,
-#     )
-#
-#     slider = Slider(start=0, end=len(frames), value=0, step=1, title="frame")
-#     slider.js_on_change("value", slider_callback)
-#
-#     f.legend.location = "top_left"
-#     f.legend.click_policy = "hide"
-#
-#     layout = column(f, slider)
-#     save(layout)
+def get_frame_trajectories(frames: np.ndarray, agents_frames: List[np.ndarray], track_ids: np.ndarray,
+                           frame_index: int) -> List[TrajectoryVisualisation]:
+
+    traj_visualisation: List[TrajectoryVisualisation] = []
+    # TODO: factor out future length
+    agent_traj_length = 20
+    for track_id in track_ids:
+        pos, *_, avail = get_relative_poses(agent_traj_length, frames[frame_index: frame_index + agent_traj_length],
+                                            track_id, agents_frames[frame_index: frame_index + agent_traj_length],
+                                            np.eye(3), 0)
+        traj_visualisation.append(TrajectoryVisualisation(xs=pos[avail > 0, 0],
+                                                          ys=pos[avail > 0, 1],
+                                                          color="blue",
+                                                          legend_label="agent_trajectory"))
+
+    # TODO: factor out future length
+    ego_traj_length = 100
+    pos, *_, avail = get_relative_poses(ego_traj_length, frames[frame_index: frame_index + ego_traj_length],
+                                        None, agents_frames[frame_index: frame_index + ego_traj_length],
+                                        np.eye(3), 0)
+    traj_visualisation.append(TrajectoryVisualisation(xs=pos[avail > 0, 0],
+                                                      ys=pos[avail > 0, 1],
+                                                      color="red",
+                                                      legend_label="ego_trajectory"))
+
+    return traj_visualisation
 
 
-def visualise(scene_index: int, frames: List[FrameVisualisation]):
+def visualise(scene_index: int, frames: List[FrameVisualisation]) -> None:
+    """Visualise a scene using bokeh.
+
+    :param scene_index: the index of the scene, used only as the title
+    :param frames: a list of FrameVisualisation objects (one per frame of the scene)
+    :return:
+    """
     output_file("scene.html")
     agent_hover = HoverTool(
         mode="mouse",
         names=["agents"],
         tooltips=[
-            ("Type", "@name"),
-            ("Probability", "@p{0.00}%"),
-            ("Track id", "@id"),
+            ("Type", "@type"),
+            ("Probability", "@prob{0.00}%"),
+            ("Track id", "@track_id"),
         ],
     )
     out: List[Dict[str, ColumnDataSource]] = []
+
+    trajectories_labels = np.unique([traj.legend_label for frame in frames for traj in frame.trajectories])
+
     for frame_idx in range(len(frames)):
         ego = frames[frame_idx].ego
 
@@ -223,12 +138,23 @@ def visualise(scene_index: int, frames: List[FrameVisualisation]):
                 lanes_dict[k].append(v)
 
         crosswalk_dict = {k: [] for k in CWVisualisation._fields}
-        for lane in frames[frame_idx].crosswalks:
-            for k, v in zip(CWVisualisation._fields, lane):
+        for crosswalk in frames[frame_idx].crosswalks:
+            for k, v in zip(CWVisualisation._fields, crosswalk):
                 crosswalk_dict[k].append(v)
 
-        out.append(dict(ego=ColumnDataSource(ego_dict), agents=ColumnDataSource(agents_dict),
-                        lanes=ColumnDataSource(lanes_dict), crosswalks=ColumnDataSource(crosswalk_dict)))
+        # for trajectory we extract the labels so that we can show them in the legend
+        trajectory_super_dict = {label: {k: [] for k in TrajectoryVisualisation._fields}
+                                 for label in trajectories_labels}
+        for trajectory in frames[frame_idx].trajectories:
+            for k, v in zip(TrajectoryVisualisation._fields, trajectory):
+                trajectory_super_dict[trajectory.legend_label][k].append(v)
+
+        frame_dict = dict(ego=ColumnDataSource(ego_dict), agents=ColumnDataSource(agents_dict),
+                          lanes=ColumnDataSource(lanes_dict),
+                          crosswalks=ColumnDataSource(crosswalk_dict))
+        frame_dict.update({k: ColumnDataSource(v) for k, v in trajectory_super_dict.items()})
+
+        out.append(frame_dict)
 
     f = bokeh.plotting.figure(
         title="Scene {}".format(scene_index),
@@ -242,14 +168,12 @@ def visualise(scene_index: int, frames: List[FrameVisualisation]):
     f.xgrid.grid_line_color = None
     f.ygrid.grid_line_color = None
 
-    f.patches(line_width=0, alpha=0.5, source=out[0]["lanes"])
+    f.patches(line_width=0, alpha=0.5, color="color", source=out[0]["lanes"])
     f.patches(line_width=0, alpha=0.5, color="#B5B50D", source=out[0]["crosswalks"])
     f.patches(line_width=2, color="#B53331", source=out[0]["ego"])
-    f.patches(line_width=2, name="agents", source=out[0]["agents"])
+    f.patches(line_width=2, color="color", name="agents", source=out[0]["agents"])
 
-    slider_callback = CustomJS(
-        args=dict(figure=f, sources=out[0], frames=out),
-        code="""
+    js_string = """
             sources["lanes"].data = frames[cb_obj.value]["lanes"].data;
             sources["crosswalks"].data = frames[cb_obj.value]["crosswalks"].data;
             sources["agents"].data = frames[cb_obj.value]["agents"].data;
@@ -262,7 +186,18 @@ def visualise(scene_index: int, frames: List[FrameVisualisation]):
             sources["crosswalks"].change.emit();
             sources["agents"].change.emit();
             sources["ego"].change.emit();
-        """,
+
+        """
+
+    for trajectory_name in trajectories_labels:
+        f.multi_line(alpha=0.8, line_width=3, source=out[0][trajectory_name], color="color",
+                     legend_label=trajectory_name)
+        js_string += f'sources["{trajectory_name}"].data = frames[cb_obj.value]["{trajectory_name}"].data;\n' \
+                     f'sources["{trajectory_name}"].change.emit();\n'
+
+    slider_callback = CustomJS(
+        args=dict(figure=f, sources=out[0], frames=out),
+        code=js_string,
     )
 
     slider = Slider(start=0, end=len(frames), value=0, step=1, title="frame")
@@ -376,15 +311,16 @@ if __name__ == "__main__":
 
         # TODO: hardcoded threshold
         agents_frame = filter_agents_by_labels(agents_frame, 0.1)
-
         tls_frame = tls_frames[frame_idx]
+
+        traj_vis = get_frame_trajectories(frames, agents_frames, agents_frame["track_id"], frame_idx)
 
         lanes_vis, crosswalks_vis, ego_vis, agents_vis = get_frame_data(mapAPI, frame, agents_frame, tls_frame)
         out.append(FrameVisualisation(ego=ego_vis,
                                       agents=agents_vis,
                                       lanes=lanes_vis,
                                       crosswalks=crosswalks_vis,
-                                      trajectories=[]))
+                                      trajectories=traj_vis))
 
         # trajs, traj_ego = get_frame_trajectories(frames, agents_frames, agents_frame["track_id"], frame_idx)
 
