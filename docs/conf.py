a6c8031567558e2e1e3b2000d49e3b2a4bfa2dc8
Christian S. Perone <christianperone@lyft.com>
2021-08-13 16:30:34 +0000

    Adding custom style sheet.

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,8 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ["./css"]
+html_css_files = ["custom.css"]
 
 # -- Markdown support
 
