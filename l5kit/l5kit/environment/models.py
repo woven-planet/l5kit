908cd538b7d0a7feb64791346d60ce92bef8c0e4
theDebugger811 <parthkothari@lyft.com>
2021-09-03 14:29:23 +0000

    AlexNet feature extractor added

--- l5kit/l5kit/environment/models.py
+++ l5kit/l5kit/environment/models.py
@@ -21,3 +21,42 @@ def SimpleCNN_GN(num_input_channels: int, features_dim: int) -> nn.Module:
     )
 
     return model
+
+
+def AlexCNN_GN(num_input_channels: int, features_dim: int) -> nn.Module:
+    """A simplified feature extractor with GroupNorm.
+
+    :param num_input_channels: the number of input channels in the input
+    :param features_dim: the number of features to extract from input
+    """
+    model = nn.Sequential(
+        # Conv1
+        nn.Conv2d(num_input_channels, 96, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False),
+        nn.GroupNorm(6, 96),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=2, stride=2),
+        # Conv2
+        nn.Conv2d(96, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False),
+        nn.GroupNorm(8, 256),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=2, stride=2),
+        # Conv3
+        nn.Conv2d(256, 384, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False),
+        nn.GroupNorm(8, 384),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=2, stride=2),
+        # Conv4
+        nn.Conv2d(384, 384, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False),
+        nn.GroupNorm(8, 384),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=2, stride=2),
+        # Conv5
+        nn.Conv2d(384, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False),
+        nn.GroupNorm(8, 256),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=2, stride=2),
+        nn.Flatten(),
+        nn.Linear(in_features=2304, out_features=features_dim),
+    )
+
+    return model
