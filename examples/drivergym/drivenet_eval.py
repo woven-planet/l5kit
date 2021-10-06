03dc6a54367861ed0098dbf231679309be38db42
theDebugger811 <parthkothari@lyft.com>
2021-10-06 14:55:15 +0000

    DriverNet baseline eval updated

--- examples/drivergym/drivenet_eval.py
+++ examples/drivergym/drivenet_eval.py
@@ -1,27 +1,27 @@
 import torch
-from stable_baselines3.common.logger import Logger
-
+from l5kit.cle.composite_metrics import CompositeMetricAggregator
 from l5kit.cle.validators import ValidationCountingAggregator
 from l5kit.dataset import EgoDataset
+from l5kit.environment.callbacks import L5KitEvalCallback
 from l5kit.environment.gym_metric_set import CLEMetricSet
 from l5kit.simulation.dataset import SimulationConfig
 from l5kit.simulation.unroll import ClosedLoopSimulator
+from stable_baselines3.common.logger import Logger
 
 
-def calculate_ade_fde(cle_evaluator):
-    scene_metric_results = cle_evaluator.metric_results()
-    tot_ade = []
-    tot_fde = []
-    for _, metrics in scene_metric_results.items():
-        disp_error = metrics['displacement_error_l2']
-        tot_ade.append(torch.mean(disp_error[1:]).item())
-        tot_fde.append(disp_error[-1].item())
-
-    return sum(tot_ade) / len(tot_ade), sum(tot_fde) / len(tot_fde)
-
-
-def eval_model(model: torch.nn.Module, dataset: EgoDataset, logger: Logger, output_name: str,
+def eval_model(model: torch.nn.Module, dataset: EgoDataset, logger: Logger, d_set: str, iter_num: int,
                num_scenes_to_unroll: int, num_simulation_steps: int = None) -> None:
+    """Evaluator function for the drivenet model. Evaluate the model using the CLEMetricSet
+    of L5Kit. Logging is performed in the Tensorboard logger.
+
+    :param model: the trained model to evaluate
+    :param dataset: the dataset on which the models is evaluated
+    :param logger: tensorboard logger to log the evaluation results
+    :param d_set: the type of dataset being evaluated ("train" or "eval")
+    :param iter_num: iteration number of training (to log in tensorboard)
+    :param num_scenes_to_unroll: Number of scenes to evaluate in the dataset
+    :param num_simulation_steps: Number of steps to unroll the model for.
+    """
 
     model.eval()
     torch.set_grad_enabled(False)
@@ -40,31 +40,31 @@ def eval_model(model: torch.nn.Module, dataset: EgoDataset, logger: Logger, outp
 
     # metric set
     metric_set = CLEMetricSet()
-    cle_evaluator = metric_set.evaluator
-    cle_evaluator.evaluate(sim_outs)
-    ade, fde = calculate_ade_fde(cle_evaluator)
-    validation_results = cle_evaluator.validation_results()
-    agg = ValidationCountingAggregator().aggregate(validation_results)
-    cle_evaluator.reset()
+    metric_set.evaluator.evaluate(sim_outs)
 
-    d_set = output_name.split('_')[-2]
-    iter_num = output_name.split('_')[-3]
-
-    for k, v in agg.items():
-        logger.record(f'{d_set}/{k}', v.item())
-
+    # Aggregate metrics (ADE, FDE)
+    ade, fde = L5KitEvalCallback.compute_ade_fde(metric_set)
     logger.record(f'{d_set}/ade', round(ade, 3))
     logger.record(f'{d_set}/fde', round(fde, 3))
+
+    # Aggregate validators
+    validation_results = metric_set.evaluator.validation_results()
+    agg = ValidationCountingAggregator().aggregate(validation_results)
+    for k, v in agg.items():
+        logger.record(f'{d_set}/{k}', v.item())
+    # Add total collisions as well
+    tot_collision = agg['collision_front'].item() + agg['collision_side'].item() + agg['collision_rear'].item()
+    logger.record(f'{d_set}/total_collision', tot_collision)
+
+    # Aggregate composite metrics
+    composite_metric_results = metric_set.evaluator.composite_metric_results()
+    comp_agg = CompositeMetricAggregator().aggregate(composite_metric_results)
+    for k, v in comp_agg.items():
+        logger.record(f'{d_set}/{k}', v.item())
 
     # Dump log so the evaluation results are printed with the correct timestep
     logger.record("time/total timesteps", iter_num, exclude="tensorboard")
     logger.dump(iter_num)
 
-    # TODO: Import metric compute functions from L5Kit.callback.
-    # for k, v in agg.items():
-    #     print(f'{d_set}/{k}', v.item())
-
-    # print('ade: ', round(ade, 3))
-    # print('fde: ', round(fde, 3))
-
+    metric_set.evaluator.reset()
     torch.set_grad_enabled(True)
--- examples/drivergym/drivenet_train.py
+++ examples/drivergym/drivenet_train.py
@@ -1,19 +1,21 @@
+import pathlib
+
 import numpy as np
 import torch
-from drivenet_eval import eval_model
+from l5kit.configs import load_config_data
+from l5kit.data import ChunkedDataset, LocalDataManager
+from l5kit.dataset import EgoDataset
+from l5kit.kinematic import AckermanPerturbation
+from l5kit.planning.rasterized.model import RasterizedPlanningModel
+from l5kit.random import GaussianRandomGenerator
+from l5kit.rasterization import build_rasterizer
 from stable_baselines3.common import utils
 from torch import nn, optim
 from torch.utils.data import DataLoader
 from torch.utils.data.sampler import SubsetRandomSampler
 from tqdm import tqdm
 
-from l5kit.configs import load_config_data
-from l5kit.data import ChunkedDataset, LocalDataManager
-from l5kit.dataset import EgoDataset
-from l5kit.kinematic import AckermanPerturbation
-from l5kit.planning.model import PlanningModel
-from l5kit.random import GaussianRandomGenerator
-from l5kit.rasterization import build_rasterizer
+from drivenet_eval import eval_model
 
 dm = LocalDataManager(None)
 # get config
@@ -39,7 +41,7 @@ eval_dataset = EgoDataset(cfg, eval_zarr, rasterizer)
 num_scenes_to_unroll = eval_cfg["max_scene_id"]
 
 # Planning Model
-model = PlanningModel(
+model = RasterizedPlanningModel(
     model_arch="simple_cnn",
     num_input_channels=rasterizer.num_channels(),
     num_targets=3 * cfg["model_params"]["future_num_frames"],  # X, Y, Yaw * number of future states
@@ -64,7 +66,11 @@ progress_bar = tqdm(range(cfg["train_params"]["max_num_steps"]))
 losses_train = []
 model.train()
 torch.set_grad_enabled(True)
+
+# Logging and Saving
 output_name = cfg["train_params"]["output_name"]
+save_path = pathlib.Path("./checkpoints/")
+save_path.mkdir(parents=True, exist_ok=True)
 logger = utils.configure_logger(1, "./drivenet_logs/", output_name, True)
 
 for it in progress_bar:
@@ -86,29 +92,25 @@ for it in progress_bar:
     losses_train.append(loss.item())
     progress_bar.set_description(f"loss: {loss.item()} loss(avg): {np.mean(losses_train)}")
 
+    # Eval
     if (it + 1) % cfg["train_params"]["eval_every_n_steps"] == 0:
-        eval_model(model, train_dataset, logger, f"{output_name}_{it}_train_steps", num_scenes_to_unroll)
-        eval_model(model, eval_dataset, logger, f"{output_name}_{it}_eval_steps", num_scenes_to_unroll)
+        eval_model(model, train_dataset, logger, "train", it, num_scenes_to_unroll)
+        eval_model(model, eval_dataset, logger, "eval", it, num_scenes_to_unroll)
         model.train()
 
+    # Checkpoint
     if (it + 1) % cfg["train_params"]["checkpoint_every_n_steps"] == 0:
         to_save = torch.jit.script(model.cpu())
-        path_to_save = f"/code/parth/drivenet_models/{output_name}_{it}_steps.pt"
+        path_to_save = f"./checkpoints/{output_name}_{it}_steps.pt"
         to_save.save(path_to_save)
-        print(f"MODEL STORED at {path_to_save}")
         model = model.to(device)
 
-# Eval
-eval_model(model, train_dataset, logger, f"{output_name}_{max_steps}_train_steps", num_scenes_to_unroll)
-eval_model(model, eval_dataset, logger, f"{output_name}_{max_steps}_eval_steps", num_scenes_to_unroll)
+# Final Eval
+eval_model(model, train_dataset, logger, "train", max_steps, num_scenes_to_unroll)
+eval_model(model, eval_dataset, logger, "eval", max_steps, num_scenes_to_unroll)
 
-# model_path = "/code/parth/drivenet_models/dn_h3_p00_1999999_steps.pt"
-# model_path = "/code/parth/drivenet_models/dn_h0_p05_run3_1999999_steps.pt"
-# model_path = "/code/parth/drivenet_models/dn_h3_p00_run2_1199999_steps.pt"
-# model_path = "/code/parth/drivenet_models/dn_h3_p00_run3_1099999_steps.pt"
-# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-# model = torch.load(model_path).to(device)
-# model = model.eval()
-
-# eval_model(model, eval_dataset, None, "dn_h3_p00_run3_1099999_eval_steps",
-# num_scenes_to_unroll, num_simulation_steps=None)
+# Final Checkpoint
+to_save = torch.jit.script(model.cpu())
+path_to_save = f"./checkpoints/{output_name}_{max_steps}_steps.pt"
+to_save.save(path_to_save)
+model = model.to(device)
