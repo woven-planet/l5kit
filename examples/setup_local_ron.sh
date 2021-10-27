d7253df29f82a81f5d7f043b7c312339605e76e9
ronamit <amitron5@gmail.com>
2021-10-27 11:20:25 +0300

    dataset download script changes

--- examples/setup_local_ron.sh
+++ examples/setup_local_ron.sh
@@ -2,29 +2,29 @@
 
 # Make a temporary download folder
 TEMP_DOWNLOAD_DIR=$(mktemp -d)
-TEMP_DATASET_DIR=$(mktemp -d)
+DATASET_DIR=~/prediction-dataset
 
 # Download sample zarr
 echo "Downloading sample zarr dataset..."
 wget https://lyft-l5-datasets-public.s3-us-west-2.amazonaws.com/prediction/v1.1/sample.tar \
     -q --show-progress -P $TEMP_DOWNLOAD_DIR
 
-mkdir -p $TEMP_DATASET_DIR/scenes
-tar xf $TEMP_DOWNLOAD_DIR/sample.tar -C $TEMP_DATASET_DIR/scenes
+mkdir -p $DATASET_DIR/scenes
+tar xf $TEMP_DOWNLOAD_DIR/sample.tar -C $DATASET_DIR/scenes
 
 # Download semantic map
 echo "Downloading semantic map..."
 wget https://lyft-l5-datasets-public.s3-us-west-2.amazonaws.com/prediction/v1.1/semantic_map.tar \
     -q --show-progress -P $TEMP_DOWNLOAD_DIR
-mkdir -p $TEMP_DATASET_DIR/semantic_map
-tar xf $TEMP_DOWNLOAD_DIR/semantic_map.tar -C $TEMP_DATASET_DIR/semantic_map
-cp $TEMP_DATASET_DIR/semantic_map/meta.json $TEMP_DATASET_DIR/meta.json
+mkdir -p $DATASET_DIR/semantic_map
+tar xf $TEMP_DOWNLOAD_DIR/semantic_map.tar -C $DATASET_DIR/semantic_map
+cp $DATASET_DIR/semantic_map/meta.json $DATASET_DIR/meta.json
 
 # Download aerial maps
 echo "Downloading aerial maps (this can take a while)..."
 wget https://lyft-l5-datasets-public.s3-us-west-2.amazonaws.com/prediction/v1.1/aerial_map.tar \
     -q --show-progress -P $TEMP_DOWNLOAD_DIR
-tar xf $TEMP_DOWNLOAD_DIR/aerial_map.tar -C $TEMP_DATASET_DIR
+tar xf $TEMP_DOWNLOAD_DIR/aerial_map.tar -C $DATASET_DIR
 
 # Dowload sample configuration
 wget https://raw.githubusercontent.com/lyft/l5kit/master/examples/visualisation/visualisation_config.yaml -q
@@ -35,4 +35,4 @@ echo "Installing L5kit..."
 pip install --progress-bar off --quiet -U l5kit pyyaml
 
 echo "Dataset and L5kit are ready !"
-echo $TEMP_DATASET_DIR > "dataset_dir.txt"
+echo $DATASET_DIR > "dataset_dir.txt"
