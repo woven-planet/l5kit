9f8219b3301f8f3ab7aebcb0dc88dae5ff280be2
ramit <Nor64536705>
2021-11-11 15:19:14 +0200

    up

--- scenario_generation/load_data.py
+++ scenario_generation/load_data.py
@@ -8,6 +8,8 @@ from l5kit.vectorization.vectorizer_builder import build_vectorizer
 from l5kit.data import get_dataset_path
 
 def load_data(dataset_name="train_data_loader"):
+
+
     # set env variable for data
     os.environ["L5KIT_DATA_FOLDER"], project_dir = get_dataset_path()
     dm = LocalDataManager(None)
@@ -22,3 +24,5 @@ def load_data(dataset_name="train_data_loader"):
     print(dataset_vec)
     return dataset_zarr, dataset_vec, dm, cfg
 
+if __name__ == "__main__":
+    dataset_zarr, dataset_vec, dm, cfg = load_data(dataset_name="train_data_loader")
