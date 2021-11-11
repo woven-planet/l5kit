b4836bb79719be88e12dd2d2dd6f81d84dcae73b
ramit <Nor64536705>
2021-11-11 12:42:48 +0200

    up

--- scenario_generation/data_utils.py
+++ scenario_generation/data_utils.py
@@ -5,14 +5,14 @@ from l5kit.configs import load_config_data
 from l5kit.data import LocalDataManager, ChunkedDataset
 from l5kit.dataset import EgoDatasetVectorized
 from l5kit.vectorization.vectorizer_builder import build_vectorizer
+from l5kit.data import get_dataset_path
 
 def load_data(dataset_name="train_data_loader"):
     # set env variable for data
-    root_project_dir = os.path.dirname(os.getcwd())
-    os.environ["L5KIT_DATA_FOLDER"] = open(root_project_dir + "/examples/dataset_dir.txt", "r").read().strip()
+    os.environ["L5KIT_DATA_FOLDER"], project_dir = get_dataset_path()
     dm = LocalDataManager(None)
     # get config
-    cfg = load_config_data(root_project_dir + "/examples/urban_driver/config.yaml")
+    cfg = load_config_data(project_dir + "/examples/urban_driver/config.yaml")
 
     # ===== INIT DATASET
     dataset_zarr = ChunkedDataset(dm.require(cfg[dataset_name]["key"])).open()
