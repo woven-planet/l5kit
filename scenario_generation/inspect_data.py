8a612469d142647e82df192e29af2a8094d5ae89
ramitnv <ramit@nvidia.com>
2021-11-12 11:35:36 +0200

    work on data inspection

--- scenario_generation/inspect_data.py
+++ scenario_generation/inspect_data.py
@@ -24,7 +24,11 @@ def load_data(dataset_name="train_data_loader"):
     #  Get the raw  dataset object (in zarr format)
     ########################################################################
     zarr_dataset = ChunkedDataset(dm.require(cfg[dataset_name]["key"])).open()
-    """ChunkedDataset is a dataset that lives on disk in compressed chunks, it has easy to use data loading and
+    """
+    ###########################################
+    #### ChunkedDataset ####
+    ###########################################
+    ChunkedDataset is a dataset that lives on disk in compressed chunks, it has easy to use data loading and
     writing interfaces that involves making numpy-like slices.
     Currently only .zarr directory stores are supported (i.e. the data will live in a folder on your
     local filesystem called <something>.zarr).
@@ -33,7 +37,11 @@ def load_data(dataset_name="train_data_loader"):
     ########################################################################
     # Take a single scene
     ########################################################################
-    """Get a new ChunkedDataset of a single scene.
+    """
+    ###########################################
+    #### get_scene_dataset ####
+    ###########################################
+    Get a new ChunkedDataset of a single scene.
     This dataset lives in memory (as np.ndarray)
 
     :param scene_index: the scene index
@@ -44,11 +52,14 @@ def load_data(dataset_name="train_data_loader"):
 
 
     ########################################################################
-    # Transform to vectorized form and extract only initial time-step (t=0)
+    # Transform to vectorized
     ########################################################################
     # object that supports vectorization around an AV
     vectorizer = build_vectorizer(cfg, dm)
     """
+    ###########################################
+    #### build_vectorizer ####
+    ###########################################
     Get a PyTorch dataset object that can be used to train DNNs with vectorized input
     Args:
         cfg (dict): configuration file
@@ -59,18 +70,25 @@ def load_data(dataset_name="train_data_loader"):
     """
     dataset_vec = EgoDatasetVectorized(cfg, zarr_dataset, vectorizer)
 
-    # test the vectorization function
-    scene_index = 31
+
     frames = zarr_dataset.frames[get_frames_slice_from_scenes(zarr_dataset.scenes[0])]
+
+
+    ########################################################################
+    # extract only initial time-step (t=0)
+    ########################################################################
     timestep_index = 0
     sampled_data = generate_agent_sample_vectorized(timestep_index, frames, zarr_dataset.agents, zarr_dataset.tl_faces, None,
-                                                    history_num_frames_ego=0,
-                                                    history_num_frames_agents=0,
+                                                    history_num_frames_ego=1, # includes current time-step?
+                                                    history_num_frames_agents=1,
                                                     future_num_frames=1,  # we must take at least 1 to compute velocity
                                                     step_time=cfg["model_params"]["step_time"],
                                                     filter_agents_threshold=cfg["raster_params"]["filter_agents_threshold"],
                                                     vectorizer=build_vectorizer(cfg, dm))
     """
+    ###########################################
+    #### generate_agent_sample_vectorized ####
+    ###########################################
     Generates the inputs and targets to train a deep prediction model with vectorized inputs.
     A deep prediction model takes as input the state of the world in vectorized form,
     and outputs where that agent will be some seconds into the future.
@@ -102,13 +120,52 @@ def load_data(dataset_name="train_data_loader"):
         dict: a dict containing e.g. the future offset coordinates (meters),
         the future yaw angular offset, the future_availability as a binary mask,
         the vectorized input representation features, and (optional) a raster image
+        
+        Note that the vectorized features are part of the output dict:
+            vectorized_features = vectorizer.vectorize(selected_track_id, agent_centroid_m, agent_yaw_rad, agent_from_world,
+                                               history_frames, history_agents, history_tl_faces, history_coords_offset,
+                                               history_yaws_offset, history_availability, future_frames, future_agents)
+           return {**frame_info, **vectorized_features}
     """
 
+    """
+    ###########################################
+    #### vectorize ####
+    ###########################################
+    Base function to execute a vectorization process.
 
+    Arguments:
+        selected_track_id: selected_track_id: Either None for AV, or the ID of an agent that you want to
+        predict the future of.
+        This agent is centered in the representation and the returned targets are derived from their future states.
+        agent_centroid_m: position of the target agent
+        agent_yaw_rad: yaw angle of the target agent
+        agent_from_world: inverted agent pose as 3x3 matrix
+        history_frames: historical frames of the target frame
+        history_agents: agents appearing in history_frames
+        history_tl_faces: traffic light faces in history frames
+        history_position_m: historical positions of target agent
+        history_yaws_rad: historical yaws of target agent
+        history_availability: availability mask of history frames
+        future_frames: future frames of the target frame
+        future_agents: agents in future_frames
+
+    Returns:
+        dict: a dict containing the vectorized frame representation
+    """
+    sampled_data
+
+
+    ########################################################################
+    #  Transform back from vectorized representation
+    ########################################################################
 
     ########################################################################
     #  Plot initial scene
     ########################################################################
+
+
+
 
     return zarr_dataset, dataset_vec, dm, cfg
 
