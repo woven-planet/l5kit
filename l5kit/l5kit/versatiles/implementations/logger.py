3fc595eef82ee19a8c368f34ed299495552dbea3
Alborz Alavian <alborz.alavian@woven-planet.global>
2021-11-16 00:46:39 +0000

    lint

--- l5kit/l5kit/versatiles/implementations/logger.py
+++ l5kit/l5kit/versatiles/implementations/logger.py
@@ -4,7 +4,7 @@ from typing import Any, Dict
 
 def init_logger(**kwargs: Any) -> None:
     # Read level directly from kwargs, and if not available from runtime_params.
-    logging.basicConfig(level=kwargs.get("level",  kwargs.get("runtime_params", {}).get("level", "INFO")))
+    logging.basicConfig(level=kwargs.get("level", kwargs.get("runtime_params", {}).get("level", "INFO")))
 
 
 def get_logger(name: str) -> logging.LoggerAdapter:
@@ -19,7 +19,7 @@ class L5KitLogAdapter(logging.LoggerAdapter):
     def log(self, level: int, msg: str, *args: Any, **kwargs: Any) -> None:
         super().log(level, msg, *args, **kwargs)
 
-    def log_custom(self, type: str, **kwargs) -> None:
+    def log_custom(self, type: str, **kwargs: Any) -> None:
         if type not in self._custom_loggers:
             return
         self._custom_loggers[type](**kwargs)
