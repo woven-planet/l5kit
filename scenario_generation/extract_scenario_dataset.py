ece26a087accb0855b849acb70765985e54c824d
ramit <ramit@nvidia.com>
2021-11-23 10:19:08 +0200

    working on data extraction

--- scenario_generation/extract_scenario_dataset.py
+++ scenario_generation/extract_scenario_dataset.py
@@ -37,7 +37,7 @@ dataset = EgoDatasetVectorized(cfg, dataset_zarr, vectorizer)
 print(dataset)
 print(f'Dataset source: {cfg[source_dataset_name]["key"]}, number of scenes total: {n_scenes}')
 
-num_simulation_steps = 5
+num_simulation_steps = 50
 sim_cfg = SimulationConfig(use_ego_gt=False, use_agents_gt=False, disable_new_agents=True,
                            distance_th_far=500, distance_th_close=50, num_simulation_steps=num_simulation_steps,
                            start_frame_index=0, show_info=True)
@@ -61,23 +61,11 @@ frame_index = 2  # we need only the initial t, but to get the speed we need to s
 ego_input = sim_dataset.rasterise_frame_batch(frame_index)[0]
 agents_input = sim_dataset.rasterise_agents_frame_batch(frame_index)
 
-other_agents_ids = [i for i in ego_input['host_id'] if i != 0]
-
-
 agents_input_lst = []
 agents_ids_scene = [ky[1] for ky in agents_input.keys()]
 
 print('agents_ids_scene = ', agents_ids_scene)
 print('n agent in scene = ', len(agents_input))
-
-'''
-agents_feat:
-* track_id
-* type
-* centroid - # x,y position of the agent in ego coord system [m]
-* yaw - # yaw angle   of the agent in ego coord system [rad]    
-*  'extent' - [length?, width?]  [m]
-'''
 
 ego_from_world_rot_mat = ego_input['agent_from_world']
 ego_centroid = ego_input['centroid']
@@ -90,7 +78,7 @@ agents_feat = []
 agents_feat.append({'track_id': ego_input['track_id'], 'agent_type': ego_input['type'], 'yaw': 0.,
                     'centroid': np.array([0, 0]), 'speed':  ego_input['speed'], 'extent': ego_input['extent']})
 
-map_feat = []
+
 for i_agent in agents_ids_scene:
     cur_agent_in = agents_input[(scene_idx, i_agent)]
     agents_input_lst.append(cur_agent_in)
@@ -102,22 +90,41 @@ for i_agent in agents_ids_scene:
     yaw = yaw_in_world - ego_yaw
     speed = cur_agent_in['speed']
     extent = cur_agent_in['extent']
-    agents_feat.append({'track_id': track_id, 'agent_type': agent_type, 'yaw': yaw,
-                        'centroid': centroid, 'speed': speed, 'extent': extent})
-# TODO: add ego to agents_feat
-    pass
+    agents_feat.append({'track_id': track_id,
+                        'agent_type': agent_type,
+                        'yaw': yaw,  # yaw angle in the agent in ego coord system [rad]
+                        'centroid': centroid,   # x,y position of the agent in ego coord system [m]
+                        'speed': speed,  # speed [m/s ?]
+                        'extent': extent})  # [length?, width?]  [m]
+
 
 print('agents centroids: ', [af['centroid'] for af in agents_feat])
 print('agents yaws: ', [af['yaw'] for af in agents_feat])
 print('agents speed: ', [af['speed'] for af in agents_feat])
-# print('agents_feat: ', agents_feat)
-# print('map_feat: ', map_feat)
+print('agents types: ', [af['agent_type'] for af in agents_feat])
 
 # Get map features in ego coords
-map_feat.append({'lanes': ego_input['lanes']})
 
+n_elements = ego_input['lanes'].shape[1]
+map_feat = dict()
 
+fields = ['lane_left_x', 'lane_left_y', 'lane_right_x', 'lane_right_y']
+for fld in fields:
+    map_feat[fld] = []
+for i_elem in  range(n_elements):
+    n_points = ego_input['lanes_availabilities'][:, i_elem].sum()
+    if n_points == 0:
+        break
+    for fld in fields:
+        map_feat[fld].append([])
+    for i_point in range(0, n_points, 2):
+        map_feat['lane_left_x'][i_elem].append(ego_input['lanes'][i_point, i_elem, 0])
+        map_feat['lane_left_y'][i_elem].append(ego_input['lanes'][i_point, i_elem, 1])
+        map_feat['lane_right_x'][i_elem].append(ego_input['lanes'][i_point + 1, i_elem, 0])
+        map_feat['lane_right_y'][i_elem].append(ego_input['lanes'][i_point + 1, i_elem, 1])
+map_feat['n_lane_elem'] = len(map_feat['lane_left_x'])
 
+pass
 # agents_feat = dict()
 # # The coordinates (in agent reference system) of the AV in the future. Unit is meters
 # agents_feat['positions'] = ego_input['target_positions']
@@ -127,8 +134,6 @@ map_feat.append({'lanes': ego_input['lanes']})
 # # TODO: extract binary map?
 #
 # scene_save_dict = {'zarr_data': scene_dataset, 'agents_feat': agents_feat, 'map_feat': map_feat}
-
-
 
 
 ####################################################################################
@@ -148,7 +153,6 @@ show(layout)
 plotting.save(fig)
 print('Figure saved at ', figure_path)
 
-
 ####################################################################################
 # Debug plot
 ####################################################################################
@@ -158,8 +162,18 @@ X = [af['centroid'][0] for af in agents_feat]
 Y = [af['centroid'][1] for af in agents_feat]
 U = [af['speed'] * np.cos(af['yaw']) for af in agents_feat]
 V = [af['speed'] * np.sin(af['yaw']) for af in agents_feat]
-plt.figure()
-plt.quiver(X, Y, U, V, units='xy', color='b')
-plt.quiver(X[0], Y[0], U[0], V[0], units='xy', color='r')  # draw ego
-plt.grid()
+fig, ax = plt.subplots()
+ax.quiver(X, Y, U, V, units='xy', color='b')
+ax.quiver(X[0], Y[0], U[0], V[0], units='xy', color='r')  # draw ego
+
+for i_elem in range(map_feat['n_lane_elem']):
+    left_x = map_feat['lane_left_x'][i_elem]
+    left_y = map_feat['lane_left_y'][i_elem]
+    right_x = map_feat['lane_right_x'][i_elem]
+    right_y = map_feat['lane_right_y'][i_elem]
+    x = np.concatenate((left_x, right_x))
+    y = np.concatenate((left_y, right_y))
+    ax.fill(x, y, facecolor='0.4', alpha=0.2)
+ax.grid()
 plt.show()
+pass
