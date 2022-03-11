72a06f89a9d427b0f9a217c29b4ca89021f315c1
ronamit <Wup31345>
2022-03-11 18:28:17 +0200

    up

--- scenario_generation/main.py
+++ scenario_generation/main.py
@@ -21,7 +21,7 @@ parser.add_argument('--source_name', type=str,
                     default='train_data_loader',
                     help=' "train_data_loader | "val_data_loader" ')
 parser.add_argument('--verbose', type=int,
-                    default=1,
+                    default=0,
                     help=" 0 | 1 ")
 args = parser.parse_args()
 
